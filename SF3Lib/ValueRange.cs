8858b34a67051c3f83801fcf47031a60825a3e7e
Simon Bielman <simon@simonbielman.com>
2024-10-07 19:24:28 +0200

    Added ValueRange helper struct

---
+++ SF3Lib/ValueRange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF3
+{
+    /// <summary>
+    /// Two values - a beginning and an end - with an auto-calculated range.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public struct ValueRange<T>  where T : struct
+    {
+        public ValueRange(int begin, int end)
+        {
+            Begin = begin;
+            End = end;
+            Range = end - begin;
+        }
+
+        public int Begin { get; }
+        public int End { get; }
+        public int Range { get; }
+    }
+}
