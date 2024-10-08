707e969dbcc84a58a8ad6e52b4fb425e69c08edc
Simon Bielman <simon@simonbielman.com>
2024-10-08 19:15:10 +0200

    Removed orphaned ObjectListView columns

--- IconPointerEditor/Forms/frmIconPointerEditor.Designer.cs
+++ IconPointerEditor/Forms/frmIconPointerEditor.Designer.cs
@@ -43,32 +43,6 @@
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.lvcID = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.lvcAddress = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.lvcName = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.lvcPrice = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.lvcWeaponType = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvEffectsEquip = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.lvcRequirements = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.lvcRange = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.lvcAttack = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.lvcDefense = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvAttackRank = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvSpellRank = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvPhysicalAttribute = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvUnknown1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvMonsterType = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvUnknown2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvStatType1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvStatUp1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvStatType2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvStatUp2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvStatType3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvStatUp3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvStatType4 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvStatUp4 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvSpellUse = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvSpellUseLv = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.tabItemIcons = new System.Windows.Forms.TabPage();
             this.olvItemIcons = new BrightIdeasSoftware.ObjectListView();
             this.lvcItemIconName = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
@@ -115,7 +89,7 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
@@ -123,7 +97,7 @@
             // 
             this.saveAsToolStripMenuItem.Enabled = false;
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
             this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
@@ -202,203 +176,6 @@
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
             this.toolStripSeparator1.Size = new System.Drawing.Size(145, 6);
-            // 
-            // lvcID
-            // 
-            this.lvcID.AspectName = "ID";
-            this.lvcID.AspectToStringFormat = "{0:X}";
-            this.lvcID.DisplayIndex = 0;
-            this.lvcID.IsEditable = false;
-            this.lvcID.Text = "ID";
-            this.lvcID.Width = 36;
-            // 
-            // lvcAddress
-            // 
-            this.lvcAddress.AspectName = "Address";
-            this.lvcAddress.AspectToStringFormat = "{0:X}";
-            this.lvcAddress.DisplayIndex = 1;
-            this.lvcAddress.IsEditable = false;
-            this.lvcAddress.Text = "Address";
-            this.lvcAddress.Width = 54;
-            // 
-            // lvcName
-            // 
-            this.lvcName.AspectName = "Name";
-            this.lvcName.DisplayIndex = 2;
-            this.lvcName.IsEditable = false;
-            this.lvcName.Text = "Item Name";
-            this.lvcName.Width = 65;
-            // 
-            // lvcPrice
-            // 
-            this.lvcPrice.AspectName = "Price";
-            this.lvcPrice.AspectToStringFormat = "";
-            this.lvcPrice.DisplayIndex = 3;
-            this.lvcPrice.Text = "Price (dec)";
-            this.lvcPrice.Width = 70;
-            // 
-            // lvcWeaponType
-            // 
-            this.lvcWeaponType.AspectName = "WeaponType";
-            this.lvcWeaponType.AspectToStringFormat = "{0:X}";
-            this.lvcWeaponType.DisplayIndex = 4;
-            this.lvcWeaponType.Text = "WeaponType";
-            this.lvcWeaponType.Width = 80;
-            // 
-            // olvEffectsEquip
-            // 
-            this.olvEffectsEquip.AspectName = "EffectsEquip";
-            this.olvEffectsEquip.AspectToStringFormat = "{0:X}";
-            this.olvEffectsEquip.DisplayIndex = 5;
-            this.olvEffectsEquip.Text = "EffectsEquip";
-            this.olvEffectsEquip.Width = 77;
-            // 
-            // lvcRequirements
-            // 
-            this.lvcRequirements.AspectName = "Requirements";
-            this.lvcRequirements.AspectToStringFormat = "{0:X}";
-            this.lvcRequirements.DisplayIndex = 6;
-            this.lvcRequirements.Text = "Requirements";
-            this.lvcRequirements.Width = 77;
-            // 
-            // lvcRange
-            // 
-            this.lvcRange.AspectName = "Range";
-            this.lvcRange.AspectToStringFormat = "{0:X}";
-            this.lvcRange.DisplayIndex = 7;
-            this.lvcRange.Text = "Range";
-            this.lvcRange.Width = 50;
-            // 
-            // lvcAttack
-            // 
-            this.lvcAttack.AspectName = "Attack";
-            this.lvcAttack.AspectToStringFormat = "{0:X}";
-            this.lvcAttack.DisplayIndex = 8;
-            this.lvcAttack.Text = "Attack";
-            this.lvcAttack.Width = 50;
-            // 
-            // lvcDefense
-            // 
-            this.lvcDefense.AspectName = "Defense";
-            this.lvcDefense.AspectToStringFormat = "{0:X}";
-            this.lvcDefense.DisplayIndex = 9;
-            this.lvcDefense.Text = "Defense";
-            this.lvcDefense.Width = 54;
-            // 
-            // olvAttackRank
-            // 
-            this.olvAttackRank.AspectName = "AttackRank";
-            this.olvAttackRank.AspectToStringFormat = "{0:X}";
-            this.olvAttackRank.DisplayIndex = 10;
-            this.olvAttackRank.Text = "AttackRank";
-            this.olvAttackRank.Width = 75;
-            // 
-            // olvSpellRank
-            // 
-            this.olvSpellRank.AspectName = "SpellRank";
-            this.olvSpellRank.AspectToStringFormat = "{0:X}";
-            this.olvSpellRank.DisplayIndex = 11;
-            this.olvSpellRank.Text = "SpellRank";
-            this.olvSpellRank.Width = 70;
-            // 
-            // olvPhysicalAttribute
-            // 
-            this.olvPhysicalAttribute.AspectName = "PhysicalAttribute";
-            this.olvPhysicalAttribute.AspectToStringFormat = "{0:X}";
-            this.olvPhysicalAttribute.DisplayIndex = 12;
-            this.olvPhysicalAttribute.Text = "Effective type 1";
-            // 
-            // olvUnknown1
-            // 
-            this.olvUnknown1.AspectName = "Unknown1";
-            this.olvUnknown1.AspectToStringFormat = "{0:X}";
-            this.olvUnknown1.DisplayIndex = 13;
-            this.olvUnknown1.Text = "Effective 1 pow";
-            // 
-            // olvMonsterType
-            // 
-            this.olvMonsterType.AspectName = "MonsterType";
-            this.olvMonsterType.AspectToStringFormat = "{0:X}";
-            this.olvMonsterType.DisplayIndex = 14;
-            this.olvMonsterType.Text = "Effective type 2";
-            // 
-            // olvUnknown2
-            // 
-            this.olvUnknown2.AspectName = "Unknown2";
-            this.olvUnknown2.AspectToStringFormat = "{0:X}";
-            this.olvUnknown2.DisplayIndex = 15;
-            this.olvUnknown2.Text = "Effective 2 pow";
-            // 
-            // olvStatType1
-            // 
-            this.olvStatType1.AspectName = "StatType1";
-            this.olvStatType1.AspectToStringFormat = "{0:X}";
-            this.olvStatType1.DisplayIndex = 16;
-            this.olvStatType1.Text = "StatType1";
-            this.olvStatType1.Width = 65;
-            // 
-            // olvStatUp1
-            // 
-            this.olvStatUp1.AspectName = "StatUp1";
-            this.olvStatUp1.AspectToStringFormat = "{0:X}";
-            this.olvStatUp1.DisplayIndex = 17;
-            this.olvStatUp1.Text = "StatUp1";
-            // 
-            // olvStatType2
-            // 
-            this.olvStatType2.AspectName = "StatType2";
-            this.olvStatType2.AspectToStringFormat = "{0:X}";
-            this.olvStatType2.DisplayIndex = 18;
-            this.olvStatType2.Text = "StatType2";
-            // 
-            // olvStatUp2
-            // 
-            this.olvStatUp2.AspectName = "StatUp2";
-            this.olvStatUp2.AspectToStringFormat = "{0:}";
-            this.olvStatUp2.DisplayIndex = 19;
-            this.olvStatUp2.Text = "StatUp2";
-            // 
-            // olvStatType3
-            // 
-            this.olvStatType3.AspectName = "StatType3";
-            this.olvStatType3.AspectToStringFormat = "{0:X}";
-            this.olvStatType3.DisplayIndex = 20;
-            this.olvStatType3.Text = "StatType3";
-            // 
-            // olvStatUp3
-            // 
-            this.olvStatUp3.AspectName = "StatUp3";
-            this.olvStatUp3.AspectToStringFormat = "{0:X}";
-            this.olvStatUp3.DisplayIndex = 21;
-            this.olvStatUp3.Text = "StatUp3";
-            // 
-            // olvStatType4
-            // 
-            this.olvStatType4.AspectName = "StatType4";
-            this.olvStatType4.AspectToStringFormat = "{0:X}";
-            this.olvStatType4.DisplayIndex = 22;
-            this.olvStatType4.Text = "StatType4";
-            // 
-            // olvStatUp4
-            // 
-            this.olvStatUp4.AspectName = "StatUp4";
-            this.olvStatUp4.AspectToStringFormat = "{0:X}";
-            this.olvStatUp4.DisplayIndex = 23;
-            this.olvStatUp4.Text = "StatUp4";
-            // 
-            // olvSpellUse
-            // 
-            this.olvSpellUse.AspectName = "SpellUse";
-            this.olvSpellUse.AspectToStringFormat = "{0:X}";
-            this.olvSpellUse.DisplayIndex = 24;
-            this.olvSpellUse.Text = "SpellUse";
-            // 
-            // olvSpellUseLv
-            // 
-            this.olvSpellUseLv.AspectName = "SpellUseLv";
-            this.olvSpellUseLv.AspectToStringFormat = "{0:X}";
-            this.olvSpellUseLv.DisplayIndex = 25;
-            this.olvSpellUseLv.Text = "SpellUseLv";
             // 
             // tabItemIcons
             // 
@@ -508,7 +285,7 @@
             this.olvSpellIcons.GridLines = true;
             this.olvSpellIcons.HasCollapsibleGroups = false;
             this.olvSpellIcons.HideSelection = false;
-            this.olvSpellIcons.Location = new System.Drawing.Point(3, 3);
+            this.olvSpellIcons.Location = new System.Drawing.Point(0, 0);
             this.olvSpellIcons.MenuLabelGroupBy = "";
             this.olvSpellIcons.Name = "olvSpellIcons";
             this.olvSpellIcons.ShowGroups = false;
@@ -595,32 +372,6 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private BrightIdeasSoftware.OLVColumn lvcID;
-        private BrightIdeasSoftware.OLVColumn lvcAddress;
-        private BrightIdeasSoftware.OLVColumn lvcName;
-        private BrightIdeasSoftware.OLVColumn lvcPrice;
-        private BrightIdeasSoftware.OLVColumn lvcWeaponType;
-        private BrightIdeasSoftware.OLVColumn olvEffectsEquip;
-        private BrightIdeasSoftware.OLVColumn lvcRequirements;
-        private BrightIdeasSoftware.OLVColumn lvcRange;
-        private BrightIdeasSoftware.OLVColumn lvcAttack;
-        private BrightIdeasSoftware.OLVColumn lvcDefense;
-        private BrightIdeasSoftware.OLVColumn olvAttackRank;
-        private BrightIdeasSoftware.OLVColumn olvSpellRank;
-        private BrightIdeasSoftware.OLVColumn olvPhysicalAttribute;
-        private BrightIdeasSoftware.OLVColumn olvUnknown1;
-        private BrightIdeasSoftware.OLVColumn olvMonsterType;
-        private BrightIdeasSoftware.OLVColumn olvUnknown2;
-        private BrightIdeasSoftware.OLVColumn olvStatType1;
-        private BrightIdeasSoftware.OLVColumn olvStatUp1;
-        private BrightIdeasSoftware.OLVColumn olvStatType2;
-        private BrightIdeasSoftware.OLVColumn olvStatUp2;
-        private BrightIdeasSoftware.OLVColumn olvStatType3;
-        private BrightIdeasSoftware.OLVColumn olvStatUp3;
-        private BrightIdeasSoftware.OLVColumn olvStatType4;
-        private BrightIdeasSoftware.OLVColumn olvStatUp4;
-        private BrightIdeasSoftware.OLVColumn olvSpellUse;
-        private BrightIdeasSoftware.OLVColumn olvSpellUseLv;
         private System.Windows.Forms.TabPage tabItemIcons;
         private BrightIdeasSoftware.ObjectListView olvItemIcons;
         private BrightIdeasSoftware.OLVColumn lvcItemIconName;
@@ -640,5 +391,3 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
     }
 }
-
-
