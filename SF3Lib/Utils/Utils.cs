7bad463b0839aa99a65ff35355ef1beb0b6e840d
Simon Bielman <simon@simonbielman.com>
2024-10-18 00:00:00 +0200

    SpellValue is now ready from the XML

--- SF3Lib/Utils/Utils.cs
+++ SF3Lib/Utils/Utils.cs
@@ -1,9 +1,7 @@
-﻿using SF3.Attributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
+using System.IO;
+using System.Xml;
 
 namespace SF3.Utils
 {
@@ -49,6 +47,41 @@ namespace SF3.Utils
                 dict.Add(value, value.Name);
             }
             return dict;
+        }
+
+        /// <summary>
+        /// Parses an XML file and returns a Dictionary of values (attribute "value") with their names (attribute "name").
+        /// </summary>
+        /// <param name="filename">XML file to parse.</param>
+        /// <returns>a Dictionary of values (attribute "value") with their names (attribute "name").</returns>
+        public static Dictionary<int, string> GetValueNameDictionaryFromXML(string filename)
+        {
+            var stream = new FileStream(filename, FileMode.Open);
+
+            var settings = new XmlReaderSettings();
+            settings.IgnoreComments = true;
+            settings.IgnoreWhitespace = true;
+
+            var xml = XmlReader.Create(stream, settings);
+            xml.Read();
+
+            var nameDict = new Dictionary<int, string>();
+            while (!xml.EOF)
+            {
+                xml.Read();
+                if (xml.HasAttributes)
+                {
+                    string valueStr = xml.GetAttribute("value");
+                    string name = xml.GetAttribute("name");
+
+                    if (valueStr != null && name != null)
+                    {
+                        nameDict.Add(Convert.ToInt32(valueStr, 16), name);
+                    }
+                }
+            }
+
+            return nameDict;
         }
     }
 }
