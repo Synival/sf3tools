dc73539147d69ac3e9b02e8911ced4123d6d2156
Simon Bielman <simon@simonbielman.com>
2024-10-19 00:00:00 +0200

    Added unit tests for X11, X21, and X26 reading

--- SF3LibTests/FileEditors/IconPointerEditorTests.cs
+++ SF3LibTests/FileEditors/IconPointerEditorTests.cs
@@ -1,22 +1,74 @@
-﻿using System;
+﻿using SF3.FileEditors;
+using SF3.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SF3LibTests.Utils;
 
 namespace SF3LibTests.FileEditors
 {
     [TestClass]
     public class IconPointerEditorTests
     {
-        [TestMethod]
-        public void ItemIcons_FirstRowHasExpectedData()
+        class TestCase
         {
+            public TestCase(string filename, ScenarioType scenario, bool isX066)
+            {
+                Filename = "../../../" + filename;
+                Scenario = scenario;
+                IsX066 = isX066;
+            }
+
+            public readonly string Filename;
+            public readonly ScenarioType Scenario;
+            public readonly bool IsX066;
         }
 
         [TestMethod]
-        public void SpellIcons_FirstRowHasExpectedData()
+        public void ItemIcons_X11_X21_HaveExpectedFirstFewRows()
         {
+            var testCases = new List<TestCase>()
+            {
+                new TestCase("TestData/S1US/X011.BIN", ScenarioType.Scenario1, false),
+                new TestCase("TestData/S1US/X021.BIN", ScenarioType.Scenario1, false),
+                new TestCase("TestData/S2/X011.BIN", ScenarioType.Scenario2, false),
+                new TestCase("TestData/S2/X021.BIN", ScenarioType.Scenario2, false),
+                new TestCase("TestData/S3/X011.BIN", ScenarioType.Scenario3, false),
+                new TestCase("TestData/S3/X021.BIN", ScenarioType.Scenario3, false),
+                new TestCase("TestData/PD/X011.BIN", ScenarioType.PremiumDisk, false),
+                new TestCase("TestData/PD/X021.BIN", ScenarioType.PremiumDisk, false),
+            };
+
+            RunTestCases(testCases, testCase =>
+            {
+                var editor = new IconPointerFileEditor(testCase.Scenario, testCase.IsX066);
+                Assert.IsTrue(editor.LoadFile(testCase.Filename));
+                Assert.AreEqual(0x26, editor.ItemIconList.Models[1].TheItemIcon);
+                Assert.AreEqual(0x18, editor.SpellIconList.Models[1].TheSpellIcon);
+            });
+        }
+
+        [TestMethod]
+        public void ItemIcons_X26_HasExpectedFirstFewRows()
+        {
+            var testCases = new List<TestCase>()
+            {
+                new TestCase("TestData/S1US/X026.BIN", ScenarioType.Scenario1, true),
+                new TestCase("TestData/S1JP/X026.BIN", ScenarioType.Scenario1, true),
+                new TestCase("TestData/S2/X026.BIN", ScenarioType.Scenario2, true),
+                new TestCase("TestData/S3/X026.BIN", ScenarioType.Scenario3, true),
+                new TestCase("TestData/PD/X026.BIN", ScenarioType.PremiumDisk, true),
+            };
+
+            RunTestCases(testCases, testCase =>
+            {
+                var editor = new IconPointerFileEditor(testCase.Scenario, testCase.IsX066);
+                Assert.IsTrue(editor.LoadFile(testCase.Filename));
+                Assert.AreEqual(0x26, editor.ItemIconList.Models[1].TheItemIcon);
+                Assert.AreEqual(0x18, editor.SpellIconList.Models[1].TheSpellIcon);
+            });
         }
     }
 }
---
+++ SF3LibTests/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF3LibTests
+{
+    public static class Utils
+    {
+        public static void RunTestCases<T>(IEnumerable<T> testCases, Action<T> test)
+        {
+            var exceptions = new List<Exception>();
+
+            foreach (var testCase in testCases)
+            {
+                try
+                {
+                    test(testCase);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+
+            if (exceptions.Any())
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
