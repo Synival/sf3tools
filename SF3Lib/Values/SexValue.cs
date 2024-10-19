e9a3f48b181706712e6c29ee4695db543b590cad
Simon Bielman <simon@simonbielman.com>
2024-10-20 00:00:00 +0200

    Migrated Sex values to Sexes.xml

--- SF3Lib/Values/SexValue.cs
+++ SF3Lib/Values/SexValue.cs
@@ -14,11 +14,7 @@ namespace SF3.Values
         public const int MinValue = 0;
         public const int MaxValue = 0xFF;
 
-        // TODO: Use resources file?
-        public static readonly Dictionary<int, string> ValueNames = new Dictionary<int, string> {
-            {0x01, "Male"},
-            {0x02, "Female"},
-        };
+        public static readonly Dictionary<int, string> ValueNames = GetValueNameDictionaryFromXML("Resources/Sexes.xml");
 
         private static readonly Dictionary<NamedValue, string> _comboBoxValues = MakeNamedValueComboBoxValues(MinValue, MaxValue, (int value) => new SexValue(value));
 
