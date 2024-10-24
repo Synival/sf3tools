3c82023ee6d2a584d561011aec44b6482c3f187f
Simon Bielman <simon@simonbielman.com>
2024-10-25 00:00:00 +0200

    Applied .editorconfig formatting to DFRTool

--- DFRTool/Program.cs
+++ DFRTool/Program.cs
@@ -1,26 +1,20 @@
 ﻿using DFRLib;
 
-namespace DFRTool
-{
-    internal class Program
-    {
-        static int Main(string[] args)
-        {
+namespace DFRTool {
+    internal class Program {
+        static int Main(string[] args) {
             // Require two arguments.
-            if (args.Length < 2)
-            {
+            if (args.Length < 2) {
                 Console.WriteLine("usage: dfrtool <originalFile> <alteredFile>");
                 return 1;
             }
 
             // Create the diff. Log and errors.
             ByteDiff diff;
-            try
-            {
+            try {
                 diff = new ByteDiff(args[0], args[1]);
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 Console.Error.WriteLine(ex.Message);
                 return 1;
             }
