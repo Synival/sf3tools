c95438f97b6d82d190a8fae5210238dd880c911b
Simon Bielman <simon@simonbielman.com>
2024-10-28 00:00:00 +0100

    Moved Stats and StatsTable

--- X033_X031_Editor/Forms/frmX033_X031_Editor.cs
+++ X033_X031_Editor/Forms/frmX033_X031_Editor.cs
@@ -5,7 +5,7 @@ using CommonLib.Statistics;
 using SF3.Editor.Extensions;
 using SF3.Editor.Forms;
 using SF3.FileEditors;
-using SF3.Models.X033_X031;
+using SF3.Models;
 using SF3.Statistics;
 using SF3.Types;
 using static SF3.Editor.Extensions.TabControlExtensions;
