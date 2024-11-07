3ca018969d9e8433613f28e8fb3229491c523955
Simon Bielman <simon@simonbielman.com>
2024-11-08 00:00:00 +0100

    Groundwork for 'create' and 'apply' commands in DFRTool

--- DFRTool/Program.cs
+++ DFRTool/Program.cs
@@ -4,30 +4,72 @@ using NDesk.Options;
 namespace DFRTool {
     internal class Program {
         private const string c_ShortUsageString =
-            "Usage: dfrtool [OPTION]... original_file altered_file\n";
+            "Usage:\n" +
+            "  dfrtool [GENERAL_OPTIONS] apply [APPLY_OPTIONS]... bin_file dfr_file\n" +
+            "  dfrtool [GENERAL_OPTIONS] create [CREATE_OPTIONS]... original_file altered_file\n";
         private const string c_ErrorUsageString =
             c_ShortUsageString +
             "Try 'dfrtool --help' for more information.\n";
         private const string c_FullUsageString =
             c_ShortUsageString +
+            "\n" +
             "Outputs text in DFR format based on a comparison between two files.\n" +
             "\n" +
-            "Options:\n" +
+            "General Options:\n" +
             "  -h, --help                print this help message\n" +
-            "  -c, --combined-appends    merges all appended changes into one row\n";
+            "\n" +
+            "Apply Options:\n" +
+            "  (none)\n" +
+            "\n" +
+            "Create Ootions:\n" +
+            "  -c, --combined-appends    merges all appended changes into one row\n" +
+            "\n";
+
+        private enum Command {
+            Apply = 0,
+            Create = 1
+        };
 
         private static int Main(string[] args) {
-            var combineAppends = false;
-            var outputHelp = false;
+            // Complain if no command was found.
+            if (args == null || args.Length == 0) {
+                Console.Error.Write(c_ErrorUsageString);
+                return 1;
+            }
 
-            var options = new OptionSet() {
-               { "c|combine-appends", v => combineAppends = true },
-               { "h|help",            v => outputHelp = true },
+            // Look for an 'apply' or 'create' keyword first.
+            Command? command = null;
+            int commandArg = -1;
+            for (int i = 0; i < args.Length; i++) {
+                if (args[i].ToLower() == "apply") {
+                    command = Command.Apply;
+                    commandArg = i;
+                    break;  
+                }
+                else if (args[i].ToLower() == "create") {
+                    command = Command.Create;
+                    commandArg = i;
+                    break;  
+                }
+            }
+
+            // If a command was supplied, split arguments up.
+            var generalArgs   = (command != null) ? args[0..commandArg] : args;
+            var remainingArgs = (command != null) ? args[(commandArg + 1)..args.Length] : [];
+
+            // Gather general options.
+            var outputHelp = false;
+            var anywhereOptions = new OptionSet() {
+               { "h|help", v => outputHelp = true },
+            };
+            var generalOptions = new OptionSet() {
+                // more options coming sometime!
             };
 
-            List<string> extra;
+            string[] extraArgsBeforeCommand;
             try {
-                extra = options.Parse(args);
+                _ = anywhereOptions.Parse(args);
+                extraArgsBeforeCommand = generalOptions.Parse(generalArgs).ToArray();
             }
             catch (Exception e) {
                 Console.WriteLine("Exception caught: " + e.Message);
@@ -35,13 +77,61 @@ namespace DFRTool {
                 return 1;
             }
 
+            // Never show errors if -h was specified anywhere.
             if (outputHelp) {
                 Console.Write(c_FullUsageString);
+                return 0;
+            }
+
+            // Complain if no command was found.
+            if (command == null) {
+                Console.Error.WriteLine("Couldn't find 'apply' or 'create' command.");
+                Console.Error.Write(c_ErrorUsageString);
+                return 1;
+            }
+
+            // There shouldn't be any unrecognized options before the command.
+            if (extraArgsBeforeCommand.Length > 0) {
+                // TODO: what arguments? maybe tell the user??
+                Console.Error.WriteLine("Unrecognized arguments before 'apply' or 'create' command.");
+                Console.Error.Write(c_ErrorUsageString);
+                return 1;
+            }
+
+            switch (command) {
+                case Command.Apply:
+                    // TODO: actual command, lawl
+                    Console.WriteLine("Sorry, bud - not implemented yet lol");
+                    return 0;
+
+                case Command.Create:
+                    return Create(remainingArgs);
+
+                default:
+                    Console.Error.WriteLine("Fatal error: unimplemented command '" + command.ToString() + "'");
+                    return 1;
+            }
+        }
+
+        private static int Create(string[] args) {
+            var combineAppends = false;
+
+            var options = new OptionSet() {
+               { "c|combine-appends", v => combineAppends = true },
+            };
+
+            string[] extra;
+            try {
+                extra = options.Parse(args).ToArray();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Exception caught: " + e.Message);
+                Console.Error.Write(c_ErrorUsageString);
                 return 1;
             }
 
             // Require two arguments.
-            if (extra.Count != 2) {
+            if (extra.Length != 2) {
                 Console.Error.Write(c_ErrorUsageString);
                 return 1;
             }
