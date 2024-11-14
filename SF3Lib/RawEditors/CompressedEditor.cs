5d534b12e09e5758b98e37742f56eae95b3c5bf1
Simon Bielman <simon@simonbielman.com>
2024-11-15 00:00:00 +0100

    Introduced CompressedEditor (which still cannot decompress)

---
+++ SF3Lib/RawEditors/CompressedEditor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using static CommonLib.Utils.Compression;
+
+namespace SF3.RawEditors {
+    public class CompressedEditor : ByteEditor, ICompressedEditor {
+        public CompressedEditor(byte[] data) : base(data) {
+            IsModifiedChanged += (s, e) => {
+                // If the compressed data is marked as modified, so should the decompressed data.
+                if (IsModified)
+                    DecompressedEditor.IsModified = true;
+                // If we still need recompression, we're still modified.
+                else
+                    IsModified |= NeedsRecompression;
+            };
+        }
+
+        public override bool SetData(byte[] data) {
+            if (!base.SetData(data))
+                return false;
+
+            var decompressedData = Decompress(data);
+            if (DecompressedEditor == null) {
+                DecompressedEditor = new ByteEditor(decompressedData);
+
+                DecompressedEditor.IsModifiedChanged += (s, e) => {
+                    // When decompressed data is marked as modified, mark that we need compression.
+                    if (DecompressedEditor.IsModified)
+                        NeedsRecompression = true;
+                    // If we still need recompression, we're still modified.
+                    else
+                        DecompressedEditor.IsModified |= NeedsRecompression;
+                };
+            }
+            else {
+                // TODO: supress any changes to 'IsModified' here using some kind of stack guard
+                DecompressedEditor.SetData(decompressedData);
+                DecompressedEditor.IsModified = false;
+            }
+            return true;
+        }
+
+        public override bool OnFinalize() {
+            if (!base.OnFinalize())
+                return false;
+            if (!DecompressedEditor.Finalize())
+                return false;
+
+            // TODO: perform compression here!!
+
+            return true;
+        }
+
+        public override void Dispose() {
+            DecompressedEditor.Dispose();
+        }
+
+        public byte[] DecompressedData => DecompressedEditor.Data;
+
+        public IByteEditor DecompressedEditor { get; private set; }
+
+        public bool _needsRecompression = false;
+        public bool NeedsRecompression {
+            get => _needsRecompression;
+            set {
+                if (_needsRecompression != value) {
+                    _needsRecompression = value;
+                    NeedsRecompressionChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public event EventHandler NeedsRecompressionChanged;
+    }
+}
---
+++ SF3Lib/RawEditors/ICompressedEditor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SF3.RawEditors {
+    /// <summary>
+    /// Editor that automatically decompresses its incoming data in a sub-editor called 'DecompressedEditor'.
+    /// Upon Finalize(), it will update its own compressed data using the decompressed snapshot.
+    /// </summary>
+    public interface ICompressedEditor : IByteEditor {
+        /// <summary>
+        /// Decompressed data generated from its incoming compressed data. Do not modify!!
+        /// </summary>
+        byte[] DecompressedData { get; }
+
+        /// <summary>
+        /// Editor for 'DecompressedData'.
+        /// </summary>
+        IByteEditor DecompressedEditor { get; }
+
+        /// <summary>
+        /// If 'true', this means the decompressed data has been modified and is out of sync
+        /// with compressed data.
+        /// </summary>
+        bool NeedsRecompression { get; }
+
+        /// <summary>
+        /// Invoked when 'NeedsRecompression' value is changed.
+        /// </summary>
+        event EventHandler NeedsRecompressionChanged;
+    }
+}
