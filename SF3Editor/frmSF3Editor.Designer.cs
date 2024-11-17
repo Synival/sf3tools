6f3202860caf98695319489131d14479e8ae3b58
Simon Bielman <simon@simonbielman.com>
2024-11-18 00:00:00 +0100

    Crude PoC for auto-generated OLV's

--- SF3Editor/frmSF3Editor.Designer.cs
+++ SF3Editor/frmSF3Editor.Designer.cs
@@ -25,18 +25,31 @@ namespace SF3Editor {
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            masterEditorControl1 = new SF3.Win.Controls.MasterEditorControl();
             SuspendLayout();
+            // 
+            // masterEditorControl1
+            // 
+            masterEditorControl1.Anchor =  System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            masterEditorControl1.BackColor = Color.MistyRose;
+            masterEditorControl1.Location = new Point(12, 12);
+            masterEditorControl1.Name = "masterEditorControl1";
+            masterEditorControl1.Size = new Size(776, 426);
+            masterEditorControl1.TabIndex = 0;
             // 
             // frmSF3Editor
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(masterEditorControl1);
             Name = "frmSF3Editor";
             Text = "SF3 Editor";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private SF3.Win.Controls.MasterEditorControl masterEditorControl1;
     }
 }
--- SF3Editor/frmSF3Editor.cs
+++ SF3Editor/frmSF3Editor.cs
@@ -1,9 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
+using BrightIdeasSoftware;
+using SF3.BulkOperations;
+using SF3.Editors.MPD;
+using SF3.NamedValues;
+using SF3.RawEditors;
+using SF3.Types;
+using SF3.Win.Extensions;
 
 namespace SF3Editor {
     public partial class frmSF3Editor : Form {
+        const string c_mpdPath = "";
+
         public frmSF3Editor() {
+            SuspendLayout();
             InitializeComponent();
+
+            var mpdEditor = MPD_Editor.Create(
+                new ByteEditor(File.ReadAllBytes(c_mpdPath)),
+                new NameGetterContext(ScenarioType.Scenario1),
+                ScenarioType.Scenario1);
+
+            var table = mpdEditor.ChunkHeader;
+            var columnType = table.Rows.GetType().GetElementType()!;
+            var columnProperties = columnType
+                .GetProperties()
+                .OrderBy(x => x.DeclaringType == columnType ? 1 : 0)
+                .ToList();
+
+            var props = columnProperties
+                .Select(x => new { Property = x, Attributes = x.GetCustomAttributes(typeof(DataMetadataAttribute), true) })
+                .Where(x => x.Attributes.Length == 1)
+                .OrderBy(x => ((DataMetadataAttribute) x.Attributes[0]).DisplayOrder)
+                .ToDictionary(x => x.Property, x => (DataMetadataAttribute) x.Attributes[0]);
+
+            var lvcColumns = new List<OLVColumn>();
+            foreach (var kv in props) {
+                var prop = kv.Key;
+                var attr = kv.Value;
+
+                if (attr.EditorCapabilities == EditorCapabilities.Hidden)
+                    continue;
+
+                var lvc = new OLVColumn("lvcTest" + prop.Name, prop.Name);
+
+                if (attr.EditorCapabilities == EditorCapabilities.Auto)
+                    lvc.IsEditable = prop.GetSetMethod() != null;
+
+                lvc.Text = attr.DisplayName ?? prop.Name;
+                if (attr.DisplayFormat != null)
+                    lvc.AspectToStringFormat = attr.DisplayFormat;
+                else if (attr.IntDisplayMode == IntDisplayMode.Hex)
+                    lvc.AspectToStringFormat = "{0:X2}";
+
+                var minWidth = TextRenderer.MeasureText(lvc.Text, lvc.HeaderFont).Width;
+                lvc.Width = Math.Max(minWidth, lvc.Width);
+
+                lvcColumns.Add(lvc);
+            }
+
+            var olv = new ObjectListView();
+            ((System.ComponentModel.ISupportInitialize) olv).BeginInit();
+            olv.AllowColumnReorder = true;
+            olv.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            olv.CellEditActivation = ObjectListView.CellEditActivateMode.SingleClick;
+            olv.FullRowSelect = true;
+            olv.GridLines = true;
+            olv.HasCollapsibleGroups = false;
+            olv.HideSelection = false;
+            olv.Location = new System.Drawing.Point(3, 3);
+            olv.MenuLabelGroupBy = "";
+            olv.Name = "TestOLV";
+            olv.ShowGroups = false;
+            olv.Size = masterEditorControl1.Size;
+            olv.TabIndex = 1;
+            olv.UseAlternatingBackColors = true;
+            olv.UseCompatibleStateImageBehavior = false;
+            olv.View = System.Windows.Forms.View.Details;
+            olv.AllColumns.AddRange(lvcColumns);
+            olv.Columns.AddRange(lvcColumns.ToArray());
+            ((System.ComponentModel.ISupportInitialize) olv).EndInit();
+
+            masterEditorControl1.Controls.Add(olv);
+            ResumeLayout();
+
+            olv.Enhance(() => mpdEditor);
+            olv.AddObjects(table.Rows);
         }
     }
 }
