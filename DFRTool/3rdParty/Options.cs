dff29dc505918cacb916b5a6a6c18483739860c5
Simon Bielman <simon@simonbielman.com>
2024-11-18 00:00:00 +0100

    Added some warning suppressions to Options.cs

--- DFRTool/3rdParty/Options.cs
+++ DFRTool/3rdParty/Options.cs
@@ -1,3 +1,15 @@
+#pragma warning disable CS0672 // Member overrides obsolete member
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8601 // Possible null reference assignment.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8603 // Possible null reference return.
+#pragma warning disable CS8604 // Possible null reference argument.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning disable CS8769 // Nullability of reference types in type of parameter doesn't match implemented member (possibly because of nullability attributes).
+#pragma warning disable SYSLIB0003 // Type or member is obsolete
+#pragma warning disable SYSLIB0051 // Type or member is obsolete
+
 //
 // Options.cs
 //
