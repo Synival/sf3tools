e6fba08a0a1534ef2df3cda82cc4eeb5c76d8cf4
Simon Bielman <simon@simonbielman.com>
2024-12-11 00:00:00 +0100

    Initial commit of ByteArraySegment with placeholder functions and tests

---
+++ CommonLib/Arrays/ByteArraySegment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CommonLib.Arrays {
+    public class ByteArraySegment : IByteArray {
+        public ByteArraySegment(IByteArray parentArray, int offset, int length) {
+            if (parentArray == null)
+                throw new ArgumentNullException(nameof(parentArray));
+            if (offset < 0 || offset > parentArray.Length)
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            if (length < 0 || offset + length > parentArray.Length)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            ParentArray = parentArray;
+            Offset = offset;
+            Length = length;
+        }
+
+        private IByteArray ParentArray { get; }
+
+        public int Offset { get; private set; }
+
+        public int Length { get; private set; }
+
+        public byte this[int index] {
+            get => (index < 0 || index >= Length) ? throw new ArgumentOutOfRangeException(nameof(index)) : ParentArray[Offset + index];
+            set {
+                if (index < 0 || index >= Length)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                ParentArray[Offset + index] = value;
+            }
+        }
+
+        public int ExpandOrContractAt(int offset, int bytesToAddOrRemove) => throw new NotImplementedException();
+        public byte[] GetDataCopy() => throw new NotImplementedException();
+        public byte[] GetDataCopyAt(int offset, int length) => throw new NotImplementedException();
+        public void Resize(int size) => throw new NotImplementedException();
+        public void ResizeAt(int offset, int currentSize, int newSize) => throw new NotImplementedException();
+        public void SetDataAtTo(int offset, byte[] data) => throw new NotImplementedException();
+        public void SetDataTo(byte[] data) => throw new NotImplementedException();
+
+        public event EventHandler Modified;
+        public event ByteArrayResizedHandler Resized;
+    }
+}
---
+++ CommonLibTests/Arrays/ByteArraySegmentTests.cs
@@ -0,0 +1,124 @@
+﻿using CommonLib.Arrays;
+
+namespace CommonLib.Tests.Arrays {
+    [TestClass]
+    public class ByteArraySegmentTests {
+        [TestMethod]
+        public void IndexerSet_ModifiesParent() {
+            var parentArray = new ByteArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            var arraySegment = new ByteArraySegment(parentArray, 3, 4);
+
+            arraySegment[2] = 100;
+            Assert.AreEqual(100, arraySegment[2]);
+            Assert.AreEqual(100, parentArray[5]);
+        }
+
+        [TestMethod]
+        public void IndexerSet_OutOfRange_ThrowsException() {
+            var parentArray = new ByteArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            var arraySegment = new ByteArraySegment(parentArray, 3, 4);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => arraySegment[-1] = 0);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => arraySegment[4] = 0);
+        }
+
+        [TestMethod]
+        public void IndexerGet_GetsParentData() {
+            var parentArray = new ByteArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            var arraySegment = new ByteArraySegment(parentArray, 3, 4);
+
+            parentArray[5] = 100;
+            Assert.AreEqual(100, arraySegment[2]);
+            Assert.AreEqual(100, parentArray[5]);
+        }
+
+        [TestMethod]
+        public void IndexerGet_OutOfRange_ThrowsException() {
+            var parentArray = new ByteArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            var arraySegment = new ByteArraySegment(parentArray, 3, 4);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => arraySegment[-1]);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => arraySegment[4]);
+        }
+
+        [TestMethod]
+        public void Resize_Contract_UpdatesLengthAndContractsParent() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Resize_Expand_UpdatesLengthAndExpandsParent() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentContracted_BeforeBeginning_MovesSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentExpanded_BeforeBeginning_MovesSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentContracted_AtBeginning_MovesSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentExpanded_AtBeginning_MovesSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentContracted_AtEnd_DoesNothingToSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentExpanded_AtEnd_DoesNothingToSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentContracted_AfterEnd_DoesNothingToSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentExpanded_AfterEnd_DoesNothingToSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentContracted_Inside_ResizesSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentExpanded_Inside_ResizesSegment() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentContracted_SpanningBeginning_ThrowsExceptionAndAbortsResize() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentExpanded_SpanningBeginning_ThrowsExceptionAndAbortsResize() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentContracted_SpanningEnd_ThrowsExceptionAndAbortsResize() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ParentExpanded_SpanningEnd_ThrowsExceptionAndAbortsResize() {
+            throw new NotImplementedException();
+        }
+    }
+}
