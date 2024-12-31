363cc4481e7e1350645b9f0b003289216cb657ab
Simon Bielman <simon@simonbielman.com>
2025-01-01 00:00:00 +0100

    Added UnknownUInt8Struct for unidentified byte tables

---
+++ SF3Lib/Models/Structs/UnknownUInt8Struct.cs
@@ -0,0 +1,17 @@
+﻿using CommonLib.Attributes;
+using SF3.RawData;
+
+namespace SF3.Models.Structs {
+    public class UnknownUInt8Struct : Struct {
+        public UnknownUInt8Struct(IByteData data, int id, string name, int address)
+        : base(data, id, name, address, 0x01) {
+        }
+
+        [BulkCopy]
+        [TableViewModelColumn(displayName: "UInt8 Value", displayFormat: "X2")]
+        public byte Value {
+            get => (byte) Data.GetByte(Address);
+            set => Data.SetByte(Address, value);
+        }
+    }
+}
---
+++ SF3Lib/Models/Tables/UnknownUInt8Table.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SF3.Models.Structs;
+using SF3.RawData;
+
+namespace SF3.Models.Tables {
+    public class UnknownUInt8Table : Table<UnknownUInt8Struct> {
+        public UnknownUInt8Table(IByteData data, int address, int count)
+        : base(data, address) {
+            MaxSize = count;
+            FormatString = "X" + MaxSize.ToString().Length;
+        }
+
+        public override bool Load() => LoadUntilMax((id, address)
+            => new UnknownUInt8Struct(Data, id, "Unknown Int8 " + id.ToString(FormatString), address));
+
+        public override int? MaxSize { get; }
+
+        private string FormatString { get; }
+    }
+}
