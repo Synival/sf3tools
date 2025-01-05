b71ca32550ed2bd6238245d3b68384f92d619985
Simon Bielman <simon@simonbielman.com>
2025-01-06 00:00:00 +0100

    Added ColorTableView for viewing palettes with live updates

---
+++ SF3LibWin/Views/ColorTableView.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using BrightIdeasSoftware;
+using CommonLib.NamedValues;
+using CommonLib.Utils;
+using SF3.Models.Tables.MPD;
+using SF3.Win.Controls;
+
+namespace SF3.Win.Views {
+    public class ColorTableView : ControlSpaceView {
+        public ColorTableView(string name, ColorTable table, INameGetterContext nameGetterContext) : base(name) {
+            Table       = table;
+            TableView   = new TableView("Table", table, nameGetterContext);
+            TextureView = new TextureView("Texture");
+        }
+
+        public override Control Create() {
+            var control = base.Create();
+            if (control == null)
+                return control;            
+
+            CreateChild(TableView, (c) => {
+                var olv = (ObjectListView) c;
+                olv.CellEditFinishing += (s, e) => {
+                    if (!e.Cancel) {
+                        e.Control.LostFocus += (s, e) => UpdateBitmap();
+                    }
+                };
+            });
+
+            CreateChild(TextureView, (c) => {
+                var tc = (TextureControl) c;
+                tc.Dock = DockStyle.Right;
+                UpdateBitmap();
+            }, false);
+
+            return control;
+        }
+
+        private void UpdateBitmap() {
+            var colorCount = Table.Rows.Length;
+            var width = (int) Math.Ceiling(Math.Sqrt(colorCount));
+            var height = (int) Math.Ceiling(colorCount / (float) width);
+
+            PaletteBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            int y = 0, x = 0;
+            using (var g = Graphics.FromImage(PaletteBitmap)) {
+                g.Clear(Color.Transparent);
+                for (var i = 0; i < colorCount; i++) {
+                    var colorChannels = PixelConversion.ABGR1555toChannels(Table.Rows[i].ColorABGR1555);
+                    colorChannels.a = 0xff;
+                    var brush = new SolidBrush(Color.FromArgb(colorChannels.a, colorChannels.r, colorChannels.g, colorChannels.b));
+                    g.FillRectangle(brush, x, y, 1, 1);
+
+                    if (++x == width) {
+                        x = 0;
+                        ++y;
+                    }
+                }
+            }
+
+            var tc = (TextureControl) (TextureView?.Control);
+            if (tc != null) {
+                tc.TextureScale = (int) Math.Ceiling(128.0f / PaletteBitmap.Width);
+                tc.TextureImage = PaletteBitmap;
+            }
+        }
+
+        public readonly ColorTable Table = null;
+        public readonly TableView TableView = null;
+        public readonly TextureView TextureView = null;
+
+        public Bitmap PaletteBitmap { get; private set; } = null;
+
+    }
+}
--- SF3LibWin/Views/MPD/PalettesView.cs
+++ SF3LibWin/Views/MPD/PalettesView.cs
@@ -10,9 +10,12 @@ namespace SF3.Win.Views.MPD {
         public override Control Create() {
             if (base.Create() == null)
                 return null;
-            CreateChild(new TableView("Light Palette", Model.LightPalette, Model.NameGetterContext));
+            CreateChild(new ColorTableView("Light Palette", Model.LightPalette, Model.NameGetterContext));
+
             for (var i = 0; i < Model.TexturePalettes.Length; i++)
-                CreateChild(new TableView("Texture Palette " + (i + 1).ToString(), Model.TexturePalettes[i], Model.NameGetterContext));
+                if (Model.TexturePalettes[i] != null)
+                    CreateChild(new ColorTableView("Texture Palette " + (i + 1).ToString(), Model.TexturePalettes[i], Model.NameGetterContext));
+
             return Control;
         }
 
