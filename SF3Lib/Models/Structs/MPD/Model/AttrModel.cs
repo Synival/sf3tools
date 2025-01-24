5f4bf80d09b341f84122e6bd16ccf7bf73746d5c
Simon Bielman <simon@simonbielman.com>
2025-01-25 00:00:00 +0100

    Added column for display for ATTR modes

--- SF3Lib/Models/Structs/MPD/Model/AttrModel.cs
+++ SF3Lib/Models/Structs/MPD/Model/AttrModel.cs
@@ -1,4 +1,5 @@
-﻿using CommonLib.Attributes;
+﻿using System;
+using CommonLib.Attributes;
 using SF3.ByteData;
 
 namespace SF3.Models.Structs.MPD.Model {
@@ -6,7 +7,7 @@ namespace SF3.Models.Structs.MPD.Model {
         public int _flagAddr;                // (Uint8) Single-sided or double-sided flag
         public int _sortAddr;                // (Uint8) Sort reference position and option settings
         public int _textureNoAddr;           // (Uint16) Texture number
-        public int _displayModeAddr;         // (Uint16) Attribute data (display mode)
+        public int _modeAddr;                // (Uint16) Mode (bitset)
         public int _colorNoAddr;             // (Uint16) Color number
         public int _gouraudShadingTableAddr; // (Uint16) Gouraud shading table
         public int _dirAddr;                 // (Uint16) Texture inversion and function number
@@ -15,7 +16,7 @@ namespace SF3.Models.Structs.MPD.Model {
             _flagAddr                = Address + 0x00; // 1 byte
             _sortAddr                = Address + 0x01; // 1 byte
             _textureNoAddr           = Address + 0x02; // 2 byte
-            _displayModeAddr         = Address + 0x04; // 2 byte
+            _modeAddr                = Address + 0x04; // 2 byte
             _colorNoAddr             = Address + 0x06; // 2 byte
             _gouraudShadingTableAddr = Address + 0x08; // 2 byte
             _dirAddr                 = Address + 0x0A; // 2 byte
@@ -44,9 +45,74 @@ namespace SF3.Models.Structs.MPD.Model {
 
         [BulkCopy]
         [TableViewModelColumn(displayOrder: 3, displayFormat: "X4")]
-        public ushort DisplayMode {
-            get => (ushort) Data.GetWord(_displayModeAddr);
-            set => Data.SetWord(_displayModeAddr, value);
+        public ushort Mode {
+            get => (ushort) Data.GetWord(_modeAddr);
+            set => Data.SetWord(_modeAddr, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 3.1f, displayName: "MSBon")]
+        public bool Mode_MSBon => (Mode & (1 << 15)) != 0;
+
+        [TableViewModelColumn(displayOrder: 3.2f, displayName: "WindowMode")]
+        public string Mode_WindowMode {
+            get {
+                var windowMode = (Mode & (3 << 9)) >> 9;
+                switch (windowMode) {
+                    case 0: return "No_Window";
+                    case 1: return "2 (invalid)";
+                    case 2: return "Window_In";
+                    case 3: return "Window_Out";
+                    default: throw new InvalidOperationException("Unreachable code");
+                }
+            }
+        }
+
+        [TableViewModelColumn(displayOrder: 3.3f, displayName: "HSSon")]
+        public bool Mode_HSSon => (Mode & (1 << 12)) != 0;
+
+        [TableViewModelColumn(displayOrder: 3.4f, displayName: "MESHon")]
+        public bool Mode_MESHon => (Mode & (1 << 8)) != 0;
+
+        [TableViewModelColumn(displayOrder: 3.4f, displayName: "ECdis")]
+        public bool Mode_ECdis => (Mode & (1 << 7)) != 0;
+
+        [TableViewModelColumn(displayOrder: 3.5f, displayName: "SPdis")]
+        public bool Mode_SPdis => (Mode & (1 << 6)) != 0;
+
+        [TableViewModelColumn(displayOrder: 3.6f, displayName: "ColorMode")]
+        public string Mode_ColorMode {
+            get {
+                var colorMode = (Mode & (0x07 << 3)) >> 3;
+                switch (colorMode) {
+                    case 0: return "CL16Bnk";
+                    case 1: return "CL16Look";
+                    case 2: return "CL64Bnk";
+                    case 3: return "CL128Bnk";
+                    case 4: return "CL256Bnk";
+                    case 5: return "CL32KRGB";
+                    case 6: return "6 (invalid)";
+                    case 7: return "7 (invalid)";
+                    default: throw new InvalidOperationException("Unreachable code");
+                }
+            }
+        }
+
+        [TableViewModelColumn(displayOrder: 3.7f, displayName: "DrawMode")]
+        public string Mode_DrawMode {
+            get {
+                var drawMode = Mode & 0x07;
+                switch (drawMode) {
+                    case 0: return "CL_Replace";
+                    case 1: return "CL_Shadow";
+                    case 2: return "CL_Half";
+                    case 3: return "CL_Trans";
+                    case 4: return "CL_Gouraud";
+                    case 5: return "5 (invalid)";
+                    case 6: return "6 (invalid)";
+                    case 7: return "7 (invalid)";
+                    default: throw new InvalidOperationException("Unreachable code");
+                }
+            }
         }
 
         [BulkCopy]
