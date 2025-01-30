3201b25846f6c274d4033bee3a67e4f157c72ef6
Simon Bielman <simon@simonbielman.com>
2025-01-31 00:00:00 +0100

    Upgraded TextureView to have images and image scaling at initialization

--- SF3LibWin/Views/TextureView.cs
+++ SF3LibWin/Views/TextureView.cs
@@ -1,15 +1,28 @@
 ﻿using System.Drawing;
+using System.Windows.Forms;
 using SF3.Win.Controls;
 
 namespace SF3.Win.Views {
     public class TextureView : ControlView<TextureControl> {
-        public TextureView(string name) : base(name) { }
+        public TextureView(string name, int textureScale = 0) : base(name) {
+            ImageScale = textureScale;
+        }
 
-        public TextureControl TextureControl => (TextureControl) Control;
+        public TextureView(string name, Image image, int textureScale = 0) : base(name) {
+            _image = image;
+            ImageScale = textureScale;
+        }
 
-        public Image Image {
-            get => TextureControl.TextureImage;
-            set => TextureControl.TextureImage = value;
+        public override Control Create() {
+            var rval = base.Create();
+
+            if (ImageScale == 0)
+                ImageScale = TextureControl.TextureScale;
+            else
+                TextureControl.TextureScale = ImageScale;
+
+            TextureControl.TextureImage = _image;
+            return rval;
         }
 
         public override void RefreshContent() {
@@ -19,6 +32,32 @@ namespace SF3.Win.Views {
             var old = TextureControl.TextureImage;
             TextureControl.TextureImage = null;
             TextureControl.TextureImage = old;
+        }
+
+        public TextureControl TextureControl => (TextureControl) Control;
+
+        private Image _image = null;
+        public Image Image {
+            get => _image;
+            set {
+                if (value != _image) {
+                    _image = value;
+                    if (TextureControl != null)
+                        TextureControl.TextureImage = value;
+                }
+            }
+        }
+
+        private int _imageScale = 0;
+        public int ImageScale {
+            get => _imageScale;
+            set {
+                if (value != _imageScale) {
+                    _imageScale = value;
+                    if (TextureControl != null)
+                        TextureControl.TextureScale = value;
+                }
+            }
         }
     }
 }
