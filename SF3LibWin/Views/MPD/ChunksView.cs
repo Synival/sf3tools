45d8869df92a8ee616bfc41dfc2fb9f2c733c7e6
Simon Bielman <simon@simonbielman.com>
2025-01-31 00:00:00 +0100

    WIP views for scroll panes

--- SF3LibWin/Views/MPD/ChunksView.cs
+++ SF3LibWin/Views/MPD/ChunksView.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Windows.Forms;
+using CommonLib.Extensions;
+using CommonLib.Imaging;
+using CommonLib.Utils;
 using SF3.Models.Files.MPD;
 
 namespace SF3.Win.Views.MPD {
@@ -42,6 +47,38 @@ namespace SF3.Win.Views.MPD {
                 foreach (var texCollection in Model.TextureCollections)
                     if (texCollection != null)
                         AddChunkView(texCollection.ChunkIndex, "Textures", (name) => new TextureChunkView(name, texCollection));
+
+            // TODO: Temporary. These are likely just scroll panes. For now, just draw every other chunk as an image.
+            foreach (var chunk in Model.ChunkData) {
+                if (chunk != null && !chunkViews.ContainsKey(chunk.Index)) {
+                    var width = Math.Min(512, chunk.DecompressedData.Length);
+                    var height = chunk.DecompressedData.Length / width;
+
+                    var imageData = chunk.DecompressedData.GetDataCopyAt(0, width * height).To2DArrayColumnMajor(width, height);
+
+                    var uniquePalettes = Model.TexturePalettes.Where(x => x != null).GroupBy(x => x.Address).Select(x => x.First()).ToArray();
+                    var palettes = uniquePalettes .Select(x => x != null ? new Palette(x.Select(x => x.ColorABGR1555).ToArray()) : null).ToArray();
+                    var bitmapDatas = palettes.Select(x => x != null ? BitmapUtils.ConvertIndexedDataToABGR8888BitmapData(imageData, x, false) : null).ToArray();
+                    var bitmapHeight = bitmapDatas.Sum(x => x != null ? height : 0);
+                    var bitmapData = new byte[width * bitmapHeight * 4];
+
+                    var pos = 0;
+                    foreach (var bd in bitmapDatas) {
+                        if (bd != null) {
+                            bd.CopyTo(bitmapData, pos);
+                            pos += bd.Length;
+                        }
+                    }
+
+                    Bitmap bitmap = null;
+                    unsafe {
+                        fixed (byte* bitmapDataPtr = bitmapData)
+                            bitmap = new Bitmap(width, bitmapHeight, width * 4, PixelFormat.Format32bppArgb, (nint) bitmapDataPtr);
+                    }
+
+                    AddChunkView(chunk.Index, "Image", (name) => new TextureView(name, bitmap, 2));
+                }
+            }
 
             // Add chunks, sorted by their chunk index.
             var chunkViewArray = chunkViews.OrderBy(x => x.Key).Select(x => x.Value).ToArray();
