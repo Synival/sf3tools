4d8f081f5fd56f328984713cc3302a72082efd90
Simon Bielman <simon@simonbielman.com>
2025-02-05 00:00:00 +0100

    Fix for tests doing double-work because of ScenarioType.Demo

--- SF3LibTests/TestDataPaths.cs
+++ SF3LibTests/TestDataPaths.cs
@@ -3,14 +3,13 @@ using SF3.Types;
 namespace SF3.Tests {
     internal static class TestDataPaths {
         private static readonly Dictionary<ScenarioType, string> ScenarioDataPath = new() {
-            { ScenarioType.Ship2, "D:/" },
             { ScenarioType.Scenario1, "D:/" },
             { ScenarioType.Scenario2, "E:/" },
             { ScenarioType.Scenario3, "F:/" },
             { ScenarioType.PremiumDisk, "G:/" },
         };
 
-        public static string ResourcePath(ScenarioType scenario, string resource = "")
-            => ScenarioDataPath[scenario] + resource;
+        public static string? ResourcePath(ScenarioType scenario, string resource = "")
+            => ScenarioDataPath.TryGetValue(scenario, out string? value) ? (value + resource) : null;
     }
 }
