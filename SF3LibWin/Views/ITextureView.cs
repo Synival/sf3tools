6a3b293e2919bd4c8d7777af6cee9619c353f30e
Simon Bielman <simon@simonbielman.com>
2025-02-09 00:00:00 +0100

    Added ITextureView

---
+++ SF3LibWin/Views/ITextureView.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows.Forms;
+using SF3.Win.Controls;
+using SF3.Win.Extensions;
+
+namespace SF3.Win.Views {
+    public class ITextureView : ControlView<TextureControl> {
+        public ITextureView(string name, int textureScale = 0) : base(name) {
+            ImageScale = textureScale;
+        }
+
+        public ITextureView(string name, ITexture texture, int textureScale = 0) : base(name) {
+            _texture = texture;
+            ImageScale = textureScale;
+        }
+
+        public override Control Create() {
+            var rval = base.Create();
+
+            if (ImageScale == 0)
+                ImageScale = TextureControl.TextureScale;
+            else
+                TextureControl.TextureScale = ImageScale;
+
+            TextureControl.TextureImage = _texture?.CreateBitmapARGB1555();
+            return rval;
+        }
+
+        public override void RefreshContent() {
+            if (!IsCreated)
+                return;
+
+            var old = TextureControl.TextureImage;
+            TextureControl.TextureImage = null;
+            TextureControl.TextureImage = old;
+        }
+
+        public TextureControl TextureControl => (TextureControl) Control;
+
+        private ITexture _texture = null;
+        public ITexture Image {
+            get => _texture;
+            set {
+                if (value != _texture) {
+                    _texture = value;
+                    if (TextureControl != null)
+                        TextureControl.TextureImage = value?.CreateBitmapARGB1555();
+                }
+            }
+        }
+
+        private int _imageScale = 0;
+        public int ImageScale {
+            get => _imageScale;
+            set {
+                if (value != _imageScale) {
+                    _imageScale = value;
+                    if (TextureControl != null)
+                        TextureControl.TextureScale = value;
+                }
+            }
+        }
+    }
+}
