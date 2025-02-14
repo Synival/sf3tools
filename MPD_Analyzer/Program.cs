950313d1f597cea8c3dfdbe9461a4665dd7cae22
Simon Bielman <simon@simonbielman.com>
2025-02-15 00:00:00 +0100

    Refactored ScanForErrorsAndReport() to have many subscans

--- MPD_Analyzer/Program.cs
+++ MPD_Analyzer/Program.cs
@@ -253,14 +253,57 @@ namespace MPD_Analyzer {
         }
 
         private static void ScanForErrorsAndReport(ScenarioType inputScenario, IMPD_File mpdFile) {
-            var header = mpdFile.MPDHeader[0];
-            var chunkHeaders = mpdFile.ChunkHeader;
+            var totalErrors = new List<string>();
 
+            var header = mpdFile.MPDHeader[0];
+
+            totalErrors.AddRange(ScanForCorrectScenario(inputScenario, mpdFile));
+            totalErrors.AddRange(ScanForNonZeroHeaderPadding(mpdFile));
+            totalErrors.AddRange(ScanForChunkHeaderErrors(mpdFile));
+            totalErrors.AddRange(ScanForPaletteErrors(mpdFile));
+            totalErrors.AddRange(ScanForSurfaceModelErrors(mpdFile));
+            totalErrors.AddRange(ScanForGroundErrors(mpdFile));
+            totalErrors.AddRange(ScanForSkyBoxErrors(mpdFile));
+
+            foreach (var error in totalErrors)
+                Console.WriteLine("    !!! " + error);
+        }
+
+        private static string[] ScanForCorrectScenario(ScenarioType inputScenario, IMPD_File mpdFile) {
             // Is this MPD file in the wrong format for this scenario? (Scenario 3 and Premium Disk are the same)
             // (This actually happens!)
             var expectedScenario = (inputScenario == ScenarioType.PremiumDisk) ? ScenarioType.Scenario3 : inputScenario;
-            if (mpdFile.Scenario != expectedScenario)
-                Console.WriteLine("  !!! Wrong scenario for this disc! ShouldBe=" + expectedScenario + ", Is=" + mpdFile.Scenario);
+            return mpdFile.Scenario != expectedScenario
+                ? ["Wrong scenario for this disc! ShouldBe=" + expectedScenario + ", Is=" + mpdFile.Scenario]
+                : [];
+        }
+
+        private static string[] ScanForNonZeroHeaderPadding(IMPD_File mpdFile) {
+            var header = mpdFile.MPDHeader[0];
+            var errors = new List<string>();
+
+            void CheckPadding(string prop, int value) {
+                if (value != 0)
+                    errors.Add($"{prop} has non-zero data: {value.ToString("X4")}");
+            }
+
+            CheckPadding(nameof(header.Padding1), header.Padding1);
+            CheckPadding(nameof(header.Padding2), header.Padding2);
+            CheckPadding(nameof(header.Padding3), header.Padding3);
+            CheckPadding(nameof(header.Padding4), header.Padding4);
+
+            return errors.ToArray();
+        }
+
+        private static string[] ScanForChunkHeaderErrors(IMPD_File mpdFile) {
+            var chunkHeaders = mpdFile.ChunkHeader;
+            var errors = new List<string>();
+
+            // Chunk[0] and Chunk[4] should always be empty.
+            if (chunkHeaders[0].Exists)
+                errors.Add("Chunk[0] exists!");
+            if (chunkHeaders[4].Exists)
+                errors.Add("Chunk[4] exists!");
 
             // Anything Scenario 2 or higher should have addresses for Chunk[20] and Chunk[21].
             bool shouldHaveChunk20_21 = mpdFile.Scenario >= ScenarioType.Scenario2;
@@ -268,48 +311,38 @@ namespace MPD_Analyzer {
             bool hasChunk21 = chunkHeaders[21].ChunkAddress > 0;
 
             if (shouldHaveChunk20_21 != hasChunk20)
-                Console.WriteLine("  !!! Chunk[20] problem! ShouldHave=" + shouldHaveChunk20_21 + ", DoesHave=" + hasChunk20);
+                errors.Add("Chunk[20] problem! ShouldHave=" + shouldHaveChunk20_21 + ", DoesHave=" + hasChunk20);
             if (shouldHaveChunk20_21 != hasChunk21)
-                Console.WriteLine("  !!! Chunk[21] problem! ShouldHave=" + shouldHaveChunk20_21 + ", DoesHave=" + hasChunk21);
+                errors.Add("Chunk[21] problem! ShouldHave=" + shouldHaveChunk20_21 + ", DoesHave=" + hasChunk21);
 
-            // Only a few files have both Chunk[1] and Chunk[20]. Let's log them.
-            if (chunkHeaders[1].Exists && chunkHeaders[20].Exists && mpdFile.SurfaceModelChunkIndex != 20)
-                Console.WriteLine("  Info: has Chunk[1] and Chunk[20]");
+            return errors.ToArray();
+        }
 
-            // For Scenario 2 onwards, if Chunk 2 is empty, Chunk 20 should probably have the surface data, or nothing at all.
-            if (mpdFile.Scenario >= ScenarioType.Scenario2) {
-                if (!chunkHeaders[2].Exists) {
-                    if (chunkHeaders[20].Exists && chunkHeaders[20].ChunkSize != 0xCF00)
-                        Console.WriteLine("  !!! Chunk[2] missing, Chunk[20] present, but not surface data!");
-                    if (chunkHeaders[21].Exists)
-                        Console.WriteLine("  !!! Chunk[2] missing, but Chunk[21] exists??");
-                }
-                else {
-                    if (!chunkHeaders[20].Exists) {
-                        // Three valid MPDs in Scenario 3 have Chunk[2] but no Chunk[20], but that's it.
-                        // This isn't the case in Scenario 2 or the Premium Disk.
-                        Console.WriteLine("  !!! Chunk[2] exists, but Chunk[20] should exist as well!");
-                        if (chunkHeaders[21].Exists)
-                            Console.WriteLine("  !!! Chunk[2] and Chunk[20] is missing, but Chunk[21] exists??");
-                    }
-                }
-            }
+        private static string[] ScanForPaletteErrors(IMPD_File mpdFile) {
+            var header = mpdFile.MPDHeader[0];
+            var errors = new List<string>();
 
             // Anything Scenario 3 or higher should have Palette3.
             bool shouldHavePalette3 = mpdFile.Scenario >= ScenarioType.Scenario3;
             bool hasPalette3 = header.Data.GetWord(header.Address + 0x44) == 0x0029;
             if (shouldHavePalette3 != hasPalette3)
-                Console.WriteLine("  !!! Palette3 problem! ShouldHave=" + shouldHavePalette3 + ", DoesHave=" + hasPalette3);
+                errors.Add("Palette3 problem! ShouldHave=" + shouldHavePalette3 + ", DoesHave=" + hasPalette3);
 
             // If the disc is Scenario 3, all palettes should be present.
             if (mpdFile.Scenario >= ScenarioType.Scenario3) {
                 if (mpdFile.PaletteTables[0] == null)
-                    Console.WriteLine("  !!! Scenario3 Palette[0] doesn't exist!");
+                    errors.Add("Scenario3 Palette[0] doesn't exist!");
                 if (mpdFile.PaletteTables[1] == null)
-                    Console.WriteLine("  !!! Scenario3 Palette[1] doesn't exist!");
+                    errors.Add("Scenario3 Palette[1] doesn't exist!");
                 if (mpdFile.PaletteTables[2] == null)
-                    Console.WriteLine("  !!! Scenario3 Palette[2] doesn't exist!");
+                    errors.Add("Scenario3 Palette[2] doesn't exist!");
             }
+
+            return errors.ToArray();
+        }
+
+        private static string[] ScanForSurfaceModelErrors(IMPD_File mpdFile) {
+            var errors = new List<string>();
 
             if (mpdFile.SurfaceModel != null) {
                 var corners = Enum.GetValues<CornerType>();
@@ -322,14 +355,14 @@ namespace MPD_Analyzer {
                             if (c == br)
                                 continue;
                             if (moveHeights[c] != moveHeights[br])
-                                Console.WriteLine("  !!! Mismatched corner/BR walk mesh heights (" + tile.X + ", " + tile.Y + "), " + c.ToString() + ": " + moveHeights[c] + " != " + moveHeights[br]);
+                                errors.Add("Mismatched corner/BR walk mesh heights (" + tile.X + ", " + tile.Y + "), " + c.ToString() + ": " + moveHeights[c] + " != " + moveHeights[br]);
                         }
                     }
                     else {
                         var modelHeights = corners.ToDictionary(c => c, tile.GetModelVertexHeightmap);
                         foreach (var c in corners) {
                             if (moveHeights[c] != modelHeights[c])
-                                Console.WriteLine("  !!! Mismatched walk/model mesh heights for (" + tile.X + ", " + tile.Y + "), " + c.ToString() + ": " + moveHeights[c] + " != " + modelHeights[c]);
+                                errors.Add("Mismatched walk/model mesh heights for (" + tile.X + ", " + tile.Y + "), " + c.ToString() + ": " + moveHeights[c] + " != " + modelHeights[c]);
                         }
                     }
 
@@ -338,84 +371,93 @@ namespace MPD_Analyzer {
                     if (mpdFile.Scenario >= ScenarioType.Scenario3)
                         weirdTexFlags &= ~0x03;
                     if (weirdTexFlags != 0x00)
-                        Console.WriteLine("  !!! Unhandled tile texture flags: @(" + tile.X + ", " + tile.Y + "): " + weirdTexFlags.ToString("X2"));
+                        errors.Add("Unhandled tile texture flags: @(" + tile.X + ", " + tile.Y + "): " + weirdTexFlags.ToString("X2"));
                 }
             }
 
-            if (header.Padding1 != 0 || header.Padding2 != 0 || header.Padding3 != 0 || header.Padding4 != 0) {
-                Console.WriteLine($"  !!! Padding has non-zero data:");
-                Console.WriteLine($"    1={header.Padding1}, 2={header.Padding2}, 3={header.Padding3}, 4={header.Padding4}");
-            }
+            return errors.ToArray();
+        }
 
-            // Chunk[0] and Chunk[4] should always be empty.
-            if (chunkHeaders[0].Exists)
-                Console.WriteLine("  !!! Chunk[0] exists!");
-            if (chunkHeaders[4].Exists)
-                Console.WriteLine("  !!! Chunk[4] exists!");
+        private static string[] ScanForGroundErrors(IMPD_File mpdFile) {
+            var header = mpdFile.MPDHeader[0];
+            var chunkHeaders = mpdFile.ChunkHeader;
+            var errors = new List<string>();
 
             // Check for ground chunks.
             if (header.GroundImageType == GroundImageType.Invalid)
-                Console.WriteLine("  !!! Has both HasRepeatingGround and HasTiledGround!");
+                errors.Add($"Has invalid GroundImageType: {header.GroundImageType} ({((int) header.GroundImageType).ToString("X4")})");
             else if (header.GroundImageType == GroundImageType.Repeated) {
+                // TODO: proper chunk indices
                 if (!chunkHeaders[14].Exists)
-                    Console.WriteLine("  !!! HasRepeatingGround is 'true', but Chunk[14] is missing!");
+                    errors.Add("HasRepeatingGround is 'true', but Chunk[14] is missing!");
                 if (!chunkHeaders[15].Exists)
-                    Console.WriteLine("  !!! HasRepeatingGround is 'true', but Chunk[15] is missing!");
+                    errors.Add("HasRepeatingGround is 'true', but Chunk[15] is missing!");
             }
             else if (header.GroundImageType == GroundImageType.Tiled) {
+                // TODO: proper chunk indices
                 if (!chunkHeaders[14].Exists)
-                    Console.WriteLine("  !!! HasTiledGround is 'true', but Chunk[14] is missing!");
+                    errors.Add("HasTiledGround is 'true', but Chunk[14] is missing!");
                 if (!chunkHeaders[15].Exists)
-                    Console.WriteLine("  !!! HasTiledGround is 'true', but Chunk[15] is missing!");
+                    errors.Add("HasTiledGround is 'true', but Chunk[15] is missing!");
                 if (!chunkHeaders[16].Exists)
-                    Console.WriteLine("  !!! HasTiledGround is 'true', but Chunk[16] is missing!");
+                    errors.Add("HasTiledGround is 'true', but Chunk[16] is missing!");
                 if (!chunkHeaders[19].Exists)
-                    Console.WriteLine("  !!! HasTiledGround is 'true', but Chunk[19] is missing!");
-            }
-            else if (header.BackgroundImageType.HasFlag(BackgroundImageType.Still)) {
-                if (!chunkHeaders[14].Exists)
-                    Console.WriteLine("  !!! HasBackground is 'true', but Chunk[14] is missing!");
-                if (!chunkHeaders[15].Exists)
-                    Console.WriteLine("  !!! HasBackground is 'true', but Chunk[15] is missing!");
+                    errors.Add("HasTiledGround is 'true', but Chunk[19] is missing!");
             }
             else {
+                // TODO: proper chunk indices
+                // TODO: check for backgrounds
                 if (chunkHeaders[14].Exists)
-                    Console.WriteLine("  !!! Has no ground, but Chunk[14] exists!");
+                    errors.Add("Has no ground, but Chunk[14] exists!");
                 if (chunkHeaders[15].Exists)
-                    Console.WriteLine("  !!! Has no ground, but Chunk[15] exists!");
+                    errors.Add("Has no ground, but Chunk[15] exists!");
                 if (chunkHeaders[16].Exists)
-                    Console.WriteLine("  !!! Has no ground, but Chunk[16] exists!");
+                    errors.Add("Has no ground, but Chunk[16] exists!");
                 if (chunkHeaders[19].Exists)
-                    Console.WriteLine("  !!! Has no ground, but Chunk[19] exists!");
+                    errors.Add("Has no ground, but Chunk[19] exists!");
             }
+
+            return errors.ToArray();
+        }
+
+        private static string[] ScanForSkyBoxErrors(IMPD_File mpdFile) {
+            var header = mpdFile.MPDHeader[0];
+            var chunkHeaders = mpdFile.ChunkHeader;
+            var errors = new List<string>();
 
             // Check for skybox chunks.
             if (header.HasSkyBox) {
+                // TODO: proper chunk indices
                 if (mpdFile.Scenario > ScenarioType.Scenario1 && !chunkHeaders[17].Exists && !chunkHeaders[18].Exists)
-                    Console.WriteLine("  SkyBox is elsewhere");
-                else if (mpdFile.Scenario > ScenarioType.Scenario1 && chunkHeaders[17].Exists && chunkHeaders[18].Exists)
-                    Console.WriteLine("  SkyBox is in Chunk[17] and Chunk[18]");
+                    // TODO: not an error
+                    errors.Add("(normal) SkyBox is elsewhere");
                 else {
+                    // TODO: proper chunk indices
                     if (!chunkHeaders[17].Exists)
-                        Console.WriteLine("  !!! HasSkyBox is 'true', but Chunk[17] is missing!");
+                        errors.Add("HasSkyBox is 'true', but Chunk[17] is missing!");
                     if (!chunkHeaders[18].Exists)
-                        Console.WriteLine("  !!! HasSkyBox is 'true', but Chunk[18] is missing!");
+                        errors.Add("HasSkyBox is 'true', but Chunk[18] is missing!");
                 }
             }
             else if (header.BackgroundImageType.HasFlag(BackgroundImageType.Tiled)) {
+                // TODO: proper chunk indices
                 if (!chunkHeaders[17].Exists)
-                    Console.WriteLine("  !!! HasBackground is 'true', but Chunk[17] is missing!");
+                    errors.Add("HasBackground is 'true', but Chunk[17] is missing!");
                 if (!chunkHeaders[18].Exists)
-                    Console.WriteLine("  !!! HasBackground is 'true', but Chunk[18] is missing!");
+                    errors.Add("HasBackground is 'true', but Chunk[18] is missing!");
                 if (!chunkHeaders[19].Exists)
-                    Console.WriteLine("  !!! HasBackground is 'true', but Chunk[19] is missing!");
+                    errors.Add("HasBackground is 'true', but Chunk[19] is missing!");
             }
             else {
+                // TODO: proper chunk indices
+                // TODO: check for backgrounds
                 if (chunkHeaders[17].Exists)
-                    Console.WriteLine("  !!! HasSkyBox is 'false', but Chunk[17] exists!");
+                    errors.Add("HasSkyBox is 'false', but Chunk[17] exists!");
                 if (chunkHeaders[18].Exists)
-                    Console.WriteLine("  !!! HasSkyBox is 'false', but Chunk[18] exists!");
+                    errors.Add("HasSkyBox is 'false', but Chunk[18] exists!");
             }
+
+            return errors.ToArray();
         }
     }
 }
