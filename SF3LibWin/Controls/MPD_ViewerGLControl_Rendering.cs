f08574cf3a6eb71419de532ffe225b663022a097
Simon Bielman <simon@simonbielman.com>
2025-02-21 00:00:00 +0100

    Removed some accidental debugging code

--- SF3LibWin/Controls/MPD_ViewerGLControl_Rendering.cs
+++ SF3LibWin/Controls/MPD_ViewerGLControl_Rendering.cs
@@ -625,9 +625,6 @@ namespace SF3.Win.Controls {
                 var pdater = _mpdFile.ModelCollections[0].PDatasByMemoryAddress[model.PData1];
                 var attrTable = _mpdFile.ModelCollections[0].AttrTablesByMemoryAddress[pdater.AttributesOffset];
 
-                if (attrTable.Any(x => (x.Flags & ~0x10F) != 0x00))
-                    yAdjust += 16;
-
                 modelMatrix =
                     Matrix4.CreateScale(model.ScaleX * scaleAdjust, model.ScaleY * scaleAdjust, model.ScaleZ * scaleAdjust) *
                     Matrix4.CreateRotationX(model.AngleX * (float) Math.PI * -2.00f) *
