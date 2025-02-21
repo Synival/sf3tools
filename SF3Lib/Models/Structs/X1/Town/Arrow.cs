7704ebc8011a7b1acb23a4f4b2759402179dbe94
Simon Bielman <simon@simonbielman.com>
2025-02-22 00:00:00 +0100

    Added column layout descriptions for X1 arrows

--- SF3Lib/Models/Structs/X1/Town/Arrow.cs
+++ SF3Lib/Models/Structs/X1/Town/Arrow.cs
@@ -20,36 +20,42 @@ namespace SF3.Models.Structs.X1.Town {
             unknownA  = Address + 0x0a;
         }
 
+        [TableViewModelColumn(displayOrder: 0, displayName: "+0x00", displayFormat: "X2")]
         [BulkCopy]
         public int ArrowUnknown0 {
             get => Data.GetWord(unknown0);
             set => Data.SetWord(unknown0, value);
         }
 
+        [TableViewModelColumn(displayOrder: 1, displayName: "TextID", displayFormat: "X4")]
         [BulkCopy]
         public int ArrowText {
             get => Data.GetWord(textID);
             set => Data.SetWord(textID, value);
         }
 
+        [TableViewModelColumn(displayOrder: 2, displayName: "+0x04", displayFormat: "X2")]
         [BulkCopy]
         public int ArrowUnknown4 {
             get => Data.GetWord(unknown4);
             set => Data.SetWord(unknown4, value);
         }
 
+        [TableViewModelColumn(displayOrder: 3, displayName: "PointToWarpMPD", displayFormat: "X2")]
         [BulkCopy]
         public int ArrowWarp {
             get => Data.GetWord(warpInMPD);
             set => Data.SetWord(warpInMPD, value);
         }
 
+        [TableViewModelColumn(displayOrder: 4, displayName: "+0x08", displayFormat: "X2")]
         [BulkCopy]
         public int ArrowUnknown8 {
             get => Data.GetWord(unknown8);
             set => Data.SetWord(unknown8, value);
         }
 
+        [TableViewModelColumn(displayOrder: 5, displayName: "+0x0A", displayFormat: "X2")]
         [BulkCopy]
         public int ArrowUnknownA {
             get => Data.GetWord(unknownA);
