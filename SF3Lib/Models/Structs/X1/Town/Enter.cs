59ebad11ccc767b2338496bbbb3ba305b521c994
Simon Bielman <simon@simonbielman.com>
2025-02-22 00:00:00 +0100

    Added column layout descriptions for X1 non-battle entrance portals

--- SF3Lib/Models/Structs/X1/Town/Enter.cs
+++ SF3Lib/Models/Structs/X1/Town/Enter.cs
@@ -24,48 +24,56 @@ namespace SF3.Models.Structs.X1.Town {
             unknownE  = Address + 0x0e;
         }
 
+        [TableViewModelColumn(displayOrder: 0, displayName: "Scene#", displayFormat: "X2")]
         [BulkCopy]
         public int Entered {
             get => Data.GetWord(enterID);
             set => Data.SetWord(enterID, value);
         }
 
+        [TableViewModelColumn(displayOrder: 1, displayName: "+0x02", displayFormat: "X2")]
         [BulkCopy]
         public int EnterUnknown2 {
             get => Data.GetWord(unknown2);
             set => Data.SetWord(unknown2, value);
         }
 
+        [TableViewModelColumn(displayOrder: 2, displayName: "xPos")]
         [BulkCopy]
         public int EnterXPos {
             get => Data.GetWord(xPos);
             set => Data.SetWord(xPos, value);
         }
 
+        [TableViewModelColumn(displayOrder: 3, displayName: "+0x06", displayFormat: "X2")]
         [BulkCopy]
         public int EnterUnknown6 {
             get => Data.GetWord(unknown6);
             set => Data.SetWord(unknown6, value);
         }
 
+        [TableViewModelColumn(displayOrder: 4, displayName: "zPos")]
         [BulkCopy]
         public int EnterZPos {
             get => Data.GetWord(zPos);
             set => Data.SetWord(zPos, value);
         }
 
+        [TableViewModelColumn(displayOrder: 5, displayName: "Direction", displayFormat: "X4")]
         [BulkCopy]
         public int EnterDirection {
             get => Data.GetWord(direction);
             set => Data.SetWord(direction, value);
         }
 
+        [TableViewModelColumn(displayOrder: 6, displayName: "Camera", displayFormat: "X4")]
         [BulkCopy]
         public int EnterCamera {
             get => Data.GetWord(camera);
             set => Data.SetWord(camera, value);
         }
 
+        [TableViewModelColumn(displayOrder: 7, displayName: "+0x0E", displayFormat: "X2")]
         [BulkCopy]
         public int EnterUnknownE {
             get => Data.GetWord(unknownE);
