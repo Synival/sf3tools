fd58d449bb8410385d291a47a7f8764e52ba9c70
Simon Bielman <simon@simonbielman.com>
2025-02-26 00:00:00 +0100

    Updated some tab names

--- SF3LibWin/Views/X1/X1_View.cs
+++ SF3LibWin/Views/X1/X1_View.cs
@@ -17,15 +17,15 @@ namespace SF3.Win.Views.MPD {
             if (Model.BattlePointersTable != null)
                 CreateChild(new TableView("Battle Pointers", Model.BattlePointersTable, ngc));
             if (Model.NpcTable != null)
-                CreateChild(new TableView("TownNpcs", Model.NpcTable, ngc));
+                CreateChild(new TableView("Town NPCs", Model.NpcTable, ngc));
             if (Model.EnterTable != null)
                 CreateChild(new TableView("Non-Battle Enter", Model.EnterTable, ngc));
             if (Model.WarpTable != null)
-                CreateChild(new TableView("WarpTable (Scn2+)", Model.WarpTable, ngc));
+                CreateChild(new TableView("Warp Table (Scn2+)", Model.WarpTable, ngc));
             if (Model.ArrowTable != null)
                 CreateChild(new TableView("Arrows (Scn2+)", Model.ArrowTable, ngc));
             if (Model.TileMovementTable != null)
-                CreateChild(new TableView("TileData (Scn2+)", Model.TileMovementTable, ngc));
+                CreateChild(new TableView("Tile Data (Scn2+)", Model.TileMovementTable, ngc));
 
             if (Model.Battles != null) {
                 foreach (var battleKv in Model.Battles.Where(x => x.Value != null))
