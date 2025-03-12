18365a683c476862893cccbcf095359f01d1778d
Simon Bielman <simon@simonbielman.com>
2025-03-13 00:00:00 +0100

    Added supported types to FileView

--- SF3LibWin/Views/FileView.cs
+++ SF3LibWin/Views/FileView.cs
@@ -1,6 +1,16 @@
 using System.Windows.Forms;
 using SF3.Models.Files;
+using SF3.Models.Files.IconPointer;
+using SF3.Models.Files.MPD;
+using SF3.Models.Files.X005;
+using SF3.Models.Files.X012;
+using SF3.Models.Files.X014;
 using SF3.Models.Files.X1;
+using SF3.Win.Views.IconPointer;
+using SF3.Win.Views.MPD;
+using SF3.Win.Views.X005;
+using SF3.Win.Views.X012;
+using SF3.Win.Views.X014;
 using SF3.Win.Views.X1;
 
 namespace SF3.Win.Views {
@@ -12,16 +22,27 @@ namespace SF3.Win.Views {
 
         public Control Create() {
             Destroy();
-
-            switch (File) {
-                case IX1_File x1File:
-                    ActualView = new X1_View("X1_File", x1File);
-                    break;
-
-                // TODO: All supported files!
-            }
-
+            ActualView = CreateActualView();
             return ActualView?.Create();
+        }
+
+        private IView CreateActualView() {
+            switch (File) {
+                case IIconPointerFile file:
+                    return new IconPointerView("IconPointer_File", file);
+                case IX1_File file:
+                    return new X1_View("X1_File", file);
+                case IX005_File file:
+                    return new X005_View("X005_File", file);
+                case IX012_File file:
+                    return new X012_View("X012_File", file);
+                case IX014_File file:
+                    return new X014_View("X014_File", file);
+                case IMPD_File file:
+                    return new MPD_View("MPD_File", file);
+                default:
+                    return null;
+            }
         }
 
         public void Destroy() {
