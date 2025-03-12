14c02562996cac2a76f94795a8ca46f2c94924e1
Simon Bielman <simon@simonbielman.com>
2025-03-13 00:00:00 +0100

    Added proper File menu to SF3Editor

--- SF3Editor/frmSF3Editor.Designer.cs
+++ SF3Editor/frmSF3Editor.Designer.cs
@@ -25,18 +25,107 @@ namespace SF3Editor {
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            menuStrip1 = new System.Windows.Forms.MenuStrip();
+            tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            tsmiFile_Open = new System.Windows.Forms.ToolStripMenuItem();
+            tsmiFile_Save = new System.Windows.Forms.ToolStripMenuItem();
+            tsmiFile_SaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            tsmiFile_Close = new System.Windows.Forms.ToolStripMenuItem();
+            tsmiFile_Sep1 = new System.Windows.Forms.ToolStripSeparator();
+            tsmiFile_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            tsmiFile_Sep2 = new System.Windows.Forms.ToolStripSeparator();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { tsmiFile });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { tsmiFile_Open, tsmiFile_Save, tsmiFile_SaveAs, tsmiFile_Sep1, tsmiFile_Close, tsmiFile_Sep2, tsmiFile_Exit });
+            tsmiFile.Name = "tsmiFile";
+            tsmiFile.Size = new Size(37, 20);
+            tsmiFile.Text = "&File";
+            // 
+            // tsmiFile_Open
+            // 
+            tsmiFile_Open.Name = "tsmiFile_Open";
+            tsmiFile_Open.ShortcutKeys =  System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O;
+            tsmiFile_Open.Size = new Size(195, 22);
+            tsmiFile_Open.Text = "&Open...";
+            // 
+            // tsmiFile_Save
+            // 
+            tsmiFile_Save.Enabled = false;
+            tsmiFile_Save.Name = "tsmiFile_Save";
+            tsmiFile_Save.ShortcutKeys =  System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S;
+            tsmiFile_Save.Size = new Size(195, 22);
+            tsmiFile_Save.Text = "&Save";
+            // 
+            // tsmiFile_SaveAs
+            // 
+            tsmiFile_SaveAs.Enabled = false;
+            tsmiFile_SaveAs.Name = "tsmiFile_SaveAs";
+            tsmiFile_SaveAs.ShortcutKeys =  System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift | System.Windows.Forms.Keys.S;
+            tsmiFile_SaveAs.Size = new Size(195, 22);
+            tsmiFile_SaveAs.Text = "Save &As...";
+            // 
+            // tsmiFile_Close
+            // 
+            tsmiFile_Close.Enabled = false;
+            tsmiFile_Close.Name = "tsmiFile_Close";
+            tsmiFile_Close.ShortcutKeys =  System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W;
+            tsmiFile_Close.Size = new Size(195, 22);
+            tsmiFile_Close.Text = "&Close";
+            // 
+            // tsmiFile_Sep1
+            // 
+            tsmiFile_Sep1.Name = "tsmiFile_Sep1";
+            tsmiFile_Sep1.Size = new Size(192, 6);
+            // 
+            // tsmiFile_Exit
+            // 
+            tsmiFile_Exit.Name = "tsmiFile_Exit";
+            tsmiFile_Exit.ShortcutKeys =  System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4;
+            tsmiFile_Exit.Size = new Size(195, 22);
+            tsmiFile_Exit.Text = "E&xit";
+            // 
+            // tsmiFile_Sep2
+            // 
+            tsmiFile_Sep2.Name = "tsmiFile_Sep2";
+            tsmiFile_Sep2.Size = new Size(192, 6);
             // 
             // frmSF3Editor
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "frmSF3Editor";
             Text = "SF3 Editor";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile_Open;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile_Save;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile_SaveAs;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile_Close;
+        private System.Windows.Forms.ToolStripSeparator tsmiFile_Sep1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile_Exit;
+        private System.Windows.Forms.ToolStripSeparator tsmiFile_Sep2;
     }
 }
--- SF3Editor/frmSF3Editor.cs
+++ SF3Editor/frmSF3Editor.cs
@@ -19,9 +19,8 @@ using static SF3.Win.Extensions.ObjectListViewExtensions;
 
 namespace SF3Editor {
     public partial class frmSF3Editor : Form {
-        private const ScenarioType c_scenario = ScenarioType.Scenario1;
-        private const string c_discPath = "D:\\"; // Change to wherever your files are located
-        private readonly INameGetterContext c_nameGetterContext = new NameGetterContext(c_scenario);
+        private readonly Dictionary<ScenarioType, INameGetterContext> c_nameGetterContexts = Enum.GetValues<ScenarioType>()
+            .ToDictionary(x => x, x => (INameGetterContext) new NameGetterContext(x));
 
         public frmSF3Editor() {
             RegisterNamedValues();
@@ -29,40 +28,11 @@ namespace SF3Editor {
             SuspendLayout();
             InitializeComponent();
 
-            var nameGetters = Enum
-                .GetValues<ScenarioType>()
-                .ToDictionary(x => x, x => (INameGetterContext) new NameGetterContext(x));
-
-            var controlContainer = new TabView("Test Container");
-            var controlContainerControl = controlContainer.Create();
-
-            var x1sToLoad = new List<KeyValuePair<ScenarioType, string>> {
-                new KeyValuePair<ScenarioType, string>(ScenarioType.Scenario1, "D://X1ASCASL.BIN"),
-                new KeyValuePair<ScenarioType, string>(ScenarioType.Scenario1, "D://X1BTL123.BIN"),
-                new KeyValuePair<ScenarioType, string>(ScenarioType.Scenario3, "F://X1ASBRK3.BIN"),
-                new KeyValuePair<ScenarioType, string>(ScenarioType.Scenario3, "F://X1BTL323.BIN"),
-            };
-            foreach (var x1 in x1sToLoad) {
-                var scenario = x1.Key;
-                var fullPath = x1.Value;
-                var x1File = X1_File.Create(new ByteData(new ByteArray(File.ReadAllBytes(fullPath))), nameGetters[scenario], scenario, false);
-                controlContainer.CreateChild(new X1_View(Path.GetFileName(fullPath), x1File));
-            }
-
-            var iconPointerEditorBinsToLoad = new string[] {"X011.BIN", "X021.BIN"};
-            foreach (var bin in iconPointerEditorBinsToLoad) {
-                var iconPointerFile = IconPointerFile.Create(new ByteData(new ByteArray(File.ReadAllBytes(c_discPath + bin))), c_nameGetterContext, c_scenario);
-                controlContainer.CreateChild(new IconPointerView(bin, iconPointerFile));
-            }
-
-            var mpdsToLoad = new string[] {"BTL02.MPD", "BTL03.MPD", "BTL04A.MPD"};
-            foreach (var mpd in mpdsToLoad) {
-                var mpdFile = MPD_File.Create(new ByteData(new ByteArray(File.ReadAllBytes(c_discPath + mpd))), nameGetters);
-                controlContainer.CreateChild(new MPD_View(mpd, mpdFile));
-            }
-
-            controlContainerControl.Dock = DockStyle.Fill;
-            Controls.Add(controlContainerControl);
+            var fileContainer = new TabView("File Container");
+            _ = fileContainer.Create();
+            var fileContainerControl = fileContainer.TabControl;
+            fileContainerControl.Dock = DockStyle.Fill;
+            Controls.Add(fileContainerControl);
 
             ResumeLayout();
         }
