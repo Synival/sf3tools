66f3d902bb5e90c62591365c4b113afbb8c3984f
Simon Bielman <simon@simonbielman.com>
2025-03-13 00:00:00 +0100

    Added enum for supported file types

---
+++ SF3Lib/Types/SF3FileType.cs
@@ -0,0 +1,16 @@
+namespace SF3.Types {
+    public enum SF3FileType {
+        Unknown = -1,
+        X1 = 0,
+        X1BTL99,
+        X002,
+        X005,
+        X012,
+        X013,
+        X014,
+        X019,
+        X031,
+        X033,
+        MPD
+    }
+}
