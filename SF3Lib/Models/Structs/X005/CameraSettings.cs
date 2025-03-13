e0a899b0d3bcb51669153a2f8d55624c2a7a9029
Simon Bielman <simon@simonbielman.com>
2025-03-14 00:00:00 +0100

    Added camera vertical offset position and some QoL calculated values for X005 editor

--- SF3Lib/Models/Structs/X005/CameraSettings.cs
+++ SF3Lib/Models/Structs/X005/CameraSettings.cs
@@ -8,48 +8,52 @@ namespace SF3.Models.Structs.X005 {
         private readonly int _setting1DistAddr;
         private readonly int _setting2DistAddr;
         private readonly int _setting3DistAddr;
-        private readonly int _baseAngleAddr;
+        private readonly int _setting1AngleAddr;
         private readonly int _setting2AngleAdjustAddr;
         private readonly int _setting3AngleAdjustAddr;
+        private readonly int _verticalOffsetAddr;
 
         public CameraSettings(IByteData data, int id, string name, ScenarioType scenario)
         : base(data, id, name, 0x00 /* address not applicable here */, 0x00 /* size not applicable here */) {
             switch (scenario) {
                 case ScenarioType.Scenario1:
-                    _setting1DistAddr = 0x42A0;
-                    _setting2DistAddr = 0x42A8;
-                    _setting3DistAddr = 0x434C; // This doesn't seem right... AC?
-                    _baseAngleAddr    = 0x453A;
-                    // What is 0x4274?
+                    _setting1DistAddr        = 0x42A0;
+                    _setting2DistAddr        = 0x42A8;
+                    _setting3DistAddr        = 0x434C;
+                    _setting1AngleAddr       = 0x453A;
                     _setting2AngleAdjustAddr = 0x427C;
                     _setting3AngleAdjustAddr = 0x4340;
+                    _verticalOffsetAddr      = 0x4580;
                     break;
 
                 case ScenarioType.Scenario2:
-                    _setting1DistAddr = 0x4300;
-                    _setting2DistAddr = 0x4308;
-                    _setting3DistAddr = 0x430C;
-                    _baseAngleAddr    = 0x465A;
+                    _setting1DistAddr        = 0x4300;
+                    _setting2DistAddr        = 0x4308;
+                    _setting3DistAddr        = 0x430C;
+                    _setting1AngleAddr       = 0x465A;
                     _setting2AngleAdjustAddr = 0x42F4;
                     _setting3AngleAdjustAddr = 0x42F6;
+                    _verticalOffsetAddr      = 0x46A0;
                     break;
 
                 case ScenarioType.Scenario3:
-                    _setting1DistAddr = 0x42EC;
-                    _setting2DistAddr = 0x42F4;
-                    _setting3DistAddr = 0x42F8;
-                    _baseAngleAddr    = 0x4646;
+                    _setting1DistAddr        = 0x42EC;
+                    _setting2DistAddr        = 0x42F4;
+                    _setting3DistAddr        = 0x42F8;
+                    _setting1AngleAddr       = 0x4646;
                     _setting2AngleAdjustAddr = 0x42E0;
                     _setting3AngleAdjustAddr = 0x42E2;
+                    _verticalOffsetAddr      = 0x468C;
                     break;
 
                 case ScenarioType.PremiumDisk:
-                    _setting1DistAddr = 0x42FC;
-                    _setting2DistAddr = 0x4304;
-                    _setting3DistAddr = 0x4308;
-                    _baseAngleAddr    = 0x4656;
+                    _setting1DistAddr        = 0x42FC;
+                    _setting2DistAddr        = 0x4304;
+                    _setting3DistAddr        = 0x4308;
+                    _setting1AngleAddr       = 0x4656;
                     _setting2AngleAdjustAddr = 0x42F0;
                     _setting3AngleAdjustAddr = 0x42F2;
+                    _verticalOffsetAddr      = 0x469C;
                     break;
 
                 default:
@@ -80,9 +84,9 @@ namespace SF3.Models.Structs.X005 {
 
         [TableViewModelColumn(displayOrder: 3, displayFormat: "X4")]
         [BulkCopy]
-        public ushort BaseAngle {
-            get => (ushort) Data.GetWord(_baseAngleAddr);
-            set => Data.SetWord(_baseAngleAddr, value);
+        public ushort Setting1Angle {
+            get => (ushort) Data.GetWord(_setting1AngleAddr);
+            set => Data.SetWord(_setting1AngleAddr, value);
         }
 
         [TableViewModelColumn(displayOrder: 4, displayFormat: "X4")]
@@ -97,6 +101,25 @@ namespace SF3.Models.Structs.X005 {
         public ushort Setting3AngleAdjust {
             get => (ushort) Data.GetWord(_setting3AngleAdjustAddr);
             set => Data.SetWord(_setting3AngleAdjustAddr, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 6, displayFormat: "X4")]
+        [BulkCopy]
+        public ushort VerticalOffset {
+            get => (ushort) Data.GetWord(_verticalOffsetAddr);
+            set => Data.SetWord(_verticalOffsetAddr, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 3.1f, displayName: nameof(Setting2Angle) + " (calc'd)", displayFormat: "X4")]
+        public ushort Setting2Angle {
+            get => (ushort) ((short) Setting1Angle + (short) Setting2AngleAdjust);
+            set => Setting2AngleAdjust = (ushort) ((short) value - (short) Setting1Angle);
+        }
+
+        [TableViewModelColumn(displayOrder: 3.2f, displayName: nameof(Setting3Angle) + " (calc'd)", displayFormat: "X4")]
+        public ushort Setting3Angle {
+            get => (ushort) ((short) Setting1Angle + (short) Setting3AngleAdjust);
+            set => Setting3AngleAdjust = (ushort) ((short) value - (short) Setting1Angle);
         }
     }
 }
