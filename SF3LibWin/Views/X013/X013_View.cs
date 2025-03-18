007d1f420f7ef85d552e7c1c19cb1434e9254a99
Simon Bielman <simon@simonbielman.com>
2025-03-19 00:00:00 +0100

    Added X013 tables (no attributes yet)

--- SF3LibWin/Views/X013/X013_View.cs
+++ SF3LibWin/Views/X013/X013_View.cs
@@ -12,7 +12,34 @@ namespace SF3.Win.Views.X013 {
                 return null;
 
             var ngc = Model.NameGetterContext;
-            // TODO: tables!
+            if (Model.SpecialsTable != null)
+                CreateChild(new TableView("Specials", Model.SpecialsTable, ngc));
+            if (Model.SpecialEffectTable != null)
+                CreateChild(new TableView("Special Effects (Scn3+)", Model.SpecialEffectTable, ngc));
+            if (Model.FriendshipExpTable != null)
+                CreateChild(new TableView("Friendship Exp", Model.FriendshipExpTable, ngc));
+            if (Model.SupportTypeTable != null)
+                CreateChild(new TableView("Support Types", Model.SupportTypeTable, ngc));
+            if (Model.SupportStatsTable != null)
+                CreateChild(new TableView("Support Stats", Model.SupportStatsTable, ngc));
+            if (Model.SoulmateTable != null)
+                CreateChild(new TableView("Soulmate Chances", Model.SoulmateTable, ngc));
+            if (Model.SoulfailTable != null)
+                CreateChild(new TableView("Soulmate Chance Fail", Model.SoulfailTable, ngc));
+            if (Model.MagicBonusTable != null)
+                CreateChild(new TableView("Magic Bonus", Model.MagicBonusTable, ngc));
+            if (Model.CritModTable != null)
+                CreateChild(new TableView("Crit Modifiers", Model.CritModTable, ngc));
+            if (Model.SpecialChanceTable != null)
+                CreateChild(new TableView("Special Chances", Model.SpecialChanceTable, ngc));
+            if (Model.ExpLimitTable != null)
+                CreateChild(new TableView("Exp Limit", Model.ExpLimitTable, ngc));
+            if (Model.HealExpTable != null)
+                CreateChild(new TableView("Heal Exp", Model.HealExpTable, ngc));
+            if (Model.WeaponSpellRankTable != null)
+                CreateChild(new TableView("Weapon Spell Ranks", Model.WeaponSpellRankTable, ngc));
+            if (Model.StatusEffectTable != null)
+                CreateChild(new TableView("Status Effects", Model.StatusEffectTable, ngc));
 
             return Control;
         }
