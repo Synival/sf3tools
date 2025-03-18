cbcc0ee5a651050547b64d92d5b0af32de750a51
Simon Bielman <simon@simonbielman.com>
2025-03-19 00:00:00 +0100

    Added attributes for X002 Loading table

--- SF3Lib/Models/Structs/X002/Loading.cs
+++ SF3Lib/Models/Structs/X002/Loading.cs
@@ -25,12 +25,14 @@ namespace SF3.Models.Structs.X002 {
             chr        = Address + 0x0e; // 2 bytes
         }
 
+        [TableViewModelColumn(displayOrder: 0, displayName: "Location", displayFormat: "X2")]
         [BulkCopy]
         public int LocationID {
             get => Data.GetWord(locationID);
             set => Data.SetWord(locationID, value);
         }
 
+        [TableViewModelColumn(displayOrder: 1, minWidth: 140)]
         [BulkCopy]
         [NameGetter(NamedValueType.FileIndex)]
         public int X1 {
@@ -38,12 +40,14 @@ namespace SF3.Models.Structs.X002 {
             set => Data.SetWord(x1, value);
         }
 
+        [TableViewModelColumn(displayOrder: 2, displayName: "CHP? IsBattle?")]
         [BulkCopy]
         public int CHP {
             get => Data.GetWord(chp);
             set => Data.SetWord(chp, value);
         }
 
+        [TableViewModelColumn(displayOrder: 3, minWidth: 140)]
         [BulkCopy]
         [NameGetter(NamedValueType.FileIndex)]
         public int X5 {
@@ -51,12 +55,14 @@ namespace SF3.Models.Structs.X002 {
             set => Data.SetWord(x5, value);
         }
 
+        [TableViewModelColumn(displayOrder: 4, displayFormat: "X2")]
         [BulkCopy]
         public int Music {
             get => Data.GetWord(music);
             set => Data.SetWord(music, value);
         }
 
+        [TableViewModelColumn(displayOrder: 5, minWidth: 140)]
         [BulkCopy]
         [NameGetter(NamedValueType.FileIndex)]
         public int MPD {
@@ -64,12 +70,14 @@ namespace SF3.Models.Structs.X002 {
             set => Data.SetWord(mpd, value);
         }
 
+        [TableViewModelColumn(displayOrder: 6, displayName: "# of Maps?")]
         [BulkCopy]
         public int LoadUnknown {
             get => Data.GetWord(unknown);
             set => Data.SetWord(unknown, value);
         }
 
+        [TableViewModelColumn(displayOrder: 7, minWidth: 140)]
         [BulkCopy]
         [NameGetter(NamedValueType.FileIndex)]
         public int CHR {
