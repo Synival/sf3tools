1dbdc6bb6db1c0a4c1bc8d7c29bea6dcf6c50ffe
Simon Bielman <simon@simonbielman.com>
2025-04-11 00:00:00 +0200

    Bugfix: Items in tools menu weren't disabled by default

--- SF3Editor/frmSF3Editor.Designer.cs
+++ SF3Editor/frmSF3Editor.Designer.cs
@@ -222,34 +222,38 @@ namespace SF3Editor {
             // 
             // tsmiTools_ImportTable
             // 
+            tsmiTools_ImportTable.Enabled = false;
             tsmiTools_ImportTable.Name = "tsmiTools_ImportTable";
-            tsmiTools_ImportTable.Size = new Size(162, 22);
+            tsmiTools_ImportTable.Size = new Size(180, 22);
             tsmiTools_ImportTable.Text = "&Import Table...";
             tsmiTools_ImportTable.Click += tsmiTools_ImportTable_Click;
             // 
             // tsmiTools_ExportTable
             // 
+            tsmiTools_ExportTable.Enabled = false;
             tsmiTools_ExportTable.Name = "tsmiTools_ExportTable";
-            tsmiTools_ExportTable.Size = new Size(162, 22);
+            tsmiTools_ExportTable.Size = new Size(180, 22);
             tsmiTools_ExportTable.Text = "&Export Table...";
             tsmiTools_ExportTable.Click += tsmiTools_ExportTable_Click;
             // 
             // tsmiTools_Sep1
             // 
             tsmiTools_Sep1.Name = "tsmiTools_Sep1";
-            tsmiTools_Sep1.Size = new Size(159, 6);
+            tsmiTools_Sep1.Size = new Size(177, 6);
             // 
             // tsmiTools_ApplyDFR
             // 
+            tsmiTools_ApplyDFR.Enabled = false;
             tsmiTools_ApplyDFR.Name = "tsmiTools_ApplyDFR";
-            tsmiTools_ApplyDFR.Size = new Size(162, 22);
+            tsmiTools_ApplyDFR.Size = new Size(180, 22);
             tsmiTools_ApplyDFR.Text = "&Apply DFR File...";
             tsmiTools_ApplyDFR.Click += tsmiTools_ApplyDFR_Click;
             // 
             // tsmiTools_CreateDFR
             // 
+            tsmiTools_CreateDFR.Enabled = false;
             tsmiTools_CreateDFR.Name = "tsmiTools_CreateDFR";
-            tsmiTools_CreateDFR.Size = new Size(162, 22);
+            tsmiTools_CreateDFR.Size = new Size(180, 22);
             tsmiTools_CreateDFR.Text = "&Create DFR File...";
             tsmiTools_CreateDFR.Click += tsmiTools_CreateDFR_Click;
             // 
