ed12b0476f9b0b4389a1fdb3a3ab61e114a68599
Simon Bielman <simon@simonbielman.com>
2025-05-10 00:00:00 +0200

    Refactored TableArrayView to use base ArrayView

---
+++ SF3LibWin/Views/ArrayView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SF3.Win.Views {
+    public abstract class ArrayView<TElement, TView> : ControlSpaceView where TView : IView {
+        public ArrayView(string name, TElement[] elements, string keyProperty, TView elementView) : base(name) {
+            Elements = elements;
+            KeyProperty = keyProperty;
+            ElementView = elementView;
+        }
+
+        protected abstract void OnSelectValue(object sender, EventArgs args);
+
+        public override Control Create() {
+            var control = base.Create();
+            if (control == null)
+                return control;            
+
+            DropdownList = new ComboBox();
+            DropdownList.Width = 400;
+            DropdownList.DataSource = new BindingSource(Elements, null);
+            DropdownList.DisplayMember = KeyProperty;
+            DropdownList.SelectedValueChanged += OnSelectValue;
+            control.Controls.Add(DropdownList);
+
+            CreateChild(ElementView, (c) => {}, autoFill: false);
+            Control.Resize += (s, e) => {
+                var tableControl = ElementView.Control;
+                tableControl.SetBounds(0, DropdownList.Bottom + 8, Control.Width, Control.Height - DropdownList.Height - 8);
+            };
+
+            return control;
+        }
+
+        public override void Destroy() {
+            Control?.Controls.Remove(DropdownList);
+            DropdownList = null;
+            base.Destroy();
+        }
+
+        public TElement[] Elements { get; }
+        public string KeyProperty { get; }
+        public TView ElementView { get; }
+
+        public ComboBox DropdownList { get; private set; } = null;
+    }
+}
--- SF3LibWin/Views/TableArrayView.cs
+++ SF3LibWin/Views/TableArrayView.cs
@@ -1,46 +1,18 @@
-﻿using System.Windows.Forms;
+﻿using System;
 using CommonLib.NamedValues;
 using SF3.Models.Tables;
 
 namespace SF3.Win.Views {
-    public class TableArrayView<TTable> : ControlSpaceView {
-        public TableArrayView(string name, TTable[] tables, INameGetterContext nameGetterContext) : base(name) {
-            Tables = tables;
-            var elementType = typeof(TTable).GetProperty("Rows").PropertyType.GetElementType();
-            TableView = new TableView("Table", null, nameGetterContext, elementType);
+    public class TableArrayView<TTable> : ArrayView<TTable, TableView> where TTable : ITable {
+        public TableArrayView(string name, TTable[] tables, INameGetterContext nameGetterContext) : base(
+            name,
+            tables,
+            "Name",
+            new TableView("Table", null, nameGetterContext, typeof(TTable).GetProperty("Rows").PropertyType.GetElementType())
+        ) { }
+
+        protected override void OnSelectValue(object sender, EventArgs args) {
+            ElementView.Table = (TTable) DropdownList.SelectedValue;
         }
-
-        public override Control Create() {
-            var control = base.Create();
-            if (control == null)
-                return control;            
-
-            DropdownList = new ComboBox();
-            DropdownList.Width = 400;
-            DropdownList.DataSource = new BindingSource(Tables, null);
-            DropdownList.DisplayMember = "Name";
-            DropdownList.SelectedValueChanged += (s, e) => TableView.Table = (ITable) DropdownList.SelectedValue;
-            control.Controls.Add(DropdownList);
-
-            CreateChild(TableView, (c) => {}, autoFill: false);
-
-            Control.Resize += (s, e) => {
-                var tableControl = TableView.Control;
-                tableControl.SetBounds(0, DropdownList.Bottom + 8, Control.Width, Control.Height - DropdownList.Height - 8);
-            };
-
-            return control;
-        }
-
-        public override void Destroy() {
-            Control?.Controls.Remove(DropdownList);
-            DropdownList = null;
-            base.Destroy();
-        }
-
-        public TTable[] Tables { get; }
-
-        public ComboBox DropdownList { get; private set; } = null;
-        public TableView TableView { get; }
     }
 }
