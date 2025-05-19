184faf0616fd38b1db8bbadb26159bf4ef1e21ba
Simon Bielman <simon@simonbielman.com>
2025-05-20 00:00:00 +0200

    Updates to CharacterBattleSc3Model

--- SF3Lib/Models/Structs/X014/CharacterBattleSc3Model.cs
+++ SF3Lib/Models/Structs/X014/CharacterBattleSc3Model.cs
@@ -5,13 +5,13 @@ using SF3.Types;
 namespace SF3.Models.Structs.X014 {
     public class CharacterBattleSc3Model : Struct {
         private readonly int _modelFileIdAddr;
-        private readonly int _unknown0x04Addr;
-        private readonly int _unknown0x08Addr;
+        private readonly int _unknownFileOffsetAddr;
+        private readonly int _weaponModelMaskAddr;
 
         public CharacterBattleSc3Model(IByteData data, int id, string name, int address) : base(data, id, name, address, 0x0C) {
-            _modelFileIdAddr = Address + 0x00; // 4 bytes
-            _unknown0x04Addr = Address + 0x04; // 4 bytes
-            _unknown0x08Addr = Address + 0x08; // 4 bytes
+            _modelFileIdAddr       = Address + 0x00; // 4 bytes
+            _unknownFileOffsetAddr = Address + 0x04; // 4 bytes
+            _weaponModelMaskAddr   = Address + 0x08; // 4 bytes
         }
 
         [TableViewModelColumn(displayOrder: 0, displayFormat: "X3", minWidth: 120)]
@@ -24,16 +24,16 @@ namespace SF3.Models.Structs.X014 {
 
         [TableViewModelColumn(displayOrder: 1, displayFormat: "X8", minWidth: 75)]
         [BulkCopy]
-        public uint Unknown0x04 {
-            get => (uint) Data.GetDouble(_unknown0x04Addr);
-            set => Data.SetDouble(_unknown0x04Addr, (int) value);
+        public uint UnknownFileOffset {
+            get => (uint) Data.GetDouble(_unknownFileOffsetAddr);
+            set => Data.SetDouble(_unknownFileOffsetAddr, (int) value);
         }
 
         [TableViewModelColumn(displayOrder: 2, displayFormat: "X8", minWidth: 75)]
         [BulkCopy]
-        public uint Unknown0x08 {
-            get => (uint) Data.GetDouble(_unknown0x08Addr);
-            set => Data.SetDouble(_unknown0x08Addr, (int) value);
+        public uint WeaponModelMask {
+            get => (uint) Data.GetDouble(_weaponModelMaskAddr);
+            set => Data.SetDouble(_weaponModelMaskAddr, (int) value);
         }
     }
 }
