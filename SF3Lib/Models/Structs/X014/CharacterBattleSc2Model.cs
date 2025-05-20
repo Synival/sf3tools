408f03b38ea8ab0d3f1429bd892d0bca44fb2411
Simon Bielman <simon@simonbielman.com>
2025-05-21 00:00:00 +0200

    Updates to CharacterBattleSc2+3Model

--- SF3Lib/Models/Structs/X014/CharacterBattleSc2Model.cs
+++ SF3Lib/Models/Structs/X014/CharacterBattleSc2Model.cs
@@ -5,18 +5,18 @@ using SF3.Types;
 namespace SF3.Models.Structs.X014 {
     public class CharacterBattleSc2Model : Struct {
         private readonly int _modelFileIdAddr;
-        private readonly int _subModelStartAddr;
+        private readonly int _modelIndicesAddr;
 
         public CharacterBattleSc2Model(IByteData data, int id, string name, int address) : base(data, id, name, address, 0x08) {
-            _modelFileIdAddr   = Address + 0x00; // 4 bytes
-            _subModelStartAddr = Address + 0x04; // 4 bytes (8 "nibbles")
+            _modelFileIdAddr  = Address + 0x00; // 4 bytes
+            _modelIndicesAddr = Address + 0x04; // 4 bytes (8 "nibbles")
         }
 
-        private byte GetSubmodel(int index)
-            => (byte) ((Data.GetDouble(Address + 0x04) >> (index * 4)) & 0x0F);
+        private byte GetModelIndex(int promotionWeaponIndex)
+            => (byte) ((Data.GetDouble(_modelIndicesAddr) >> (promotionWeaponIndex * 4)) & 0x0F);
 
-        private void SetSubmodel(int index, byte value)
-            => Data.SetDouble(Address + 0x04, Data.GetDouble(Address + 0x04) & ~(0x0F << (index * 4)) | ((value & 0x0F) << (index * 4)));
+        private void SetModelIndex(int promotionWeaponIndex, byte value)
+            => Data.SetDouble(_modelIndicesAddr, Data.GetDouble(_modelIndicesAddr) & ~(0x0F << (promotionWeaponIndex * 4)) | ((value & 0x0F) << (promotionWeaponIndex * 4)));
 
         [TableViewModelColumn(displayOrder: 0, displayFormat: "X3", minWidth: 120)]
         [NameGetter(NamedValueType.FileIndex)]
@@ -29,57 +29,57 @@ namespace SF3.Models.Structs.X014 {
         [TableViewModelColumn(displayOrder: 1, displayFormat: "X1")]
         [BulkCopy]
         public byte UWp1 {
-            get => GetSubmodel(0);
-            set => SetSubmodel(0, value);
+            get => GetModelIndex(0);
+            set => SetModelIndex(0, value);
         }
 
         [TableViewModelColumn(displayOrder: 2, displayFormat: "X1")]
         [BulkCopy]
         public byte UWp2 {
-            get => GetSubmodel(1);
-            set => SetSubmodel(1, value);
+            get => GetModelIndex(1);
+            set => SetModelIndex(1, value);
         }
 
         [TableViewModelColumn(displayOrder: 3, displayFormat: "X1")]
         [BulkCopy]
         public byte UWp3 {
-            get => GetSubmodel(2);
-            set => SetSubmodel(2, value);
+            get => GetModelIndex(2);
+            set => SetModelIndex(2, value);
         }
 
         [TableViewModelColumn(displayOrder: 4, displayFormat: "X1")]
         [BulkCopy]
         public byte UWp4 {
-            get => GetSubmodel(3);
-            set => SetSubmodel(3, value);
+            get => GetModelIndex(3);
+            set => SetModelIndex(3, value);
         }
 
         [TableViewModelColumn(displayOrder: 5, displayFormat: "X1")]
         [BulkCopy]
         public byte PWp1 {
-            get => GetSubmodel(4);
-            set => SetSubmodel(4, value);
+            get => GetModelIndex(4);
+            set => SetModelIndex(4, value);
         }
 
         [TableViewModelColumn(displayOrder: 6, displayFormat: "X1")]
         [BulkCopy]
         public byte PWp2 {
-            get => GetSubmodel(5);
-            set => SetSubmodel(5, value);
+            get => GetModelIndex(5);
+            set => SetModelIndex(5, value);
         }
 
         [TableViewModelColumn(displayOrder: 7, displayFormat: "X1")]
         [BulkCopy]
         public byte PWp3 {
-            get => GetSubmodel(6);
-            set => SetSubmodel(6, value);
+            get => GetModelIndex(6);
+            set => SetModelIndex(6, value);
         }
 
         [TableViewModelColumn(displayOrder: 8, displayFormat: "X1")]
         [BulkCopy]
         public byte PWp4 {
-            get => GetSubmodel(7);
-            set => SetSubmodel(7, value);
+            get => GetModelIndex(7);
+            set => SetModelIndex(7, value);
         }
     }
 }
--- SF3Lib/Models/Structs/X014/CharacterBattleSc3Model.cs
+++ SF3Lib/Models/Structs/X014/CharacterBattleSc3Model.cs
@@ -5,14 +5,20 @@ using SF3.Types;
 namespace SF3.Models.Structs.X014 {
     public class CharacterBattleSc3Model : Struct {
         private readonly int _modelFileIdAddr;
-        private readonly int _unknownFileOffsetAddr;
-        private readonly int _weaponModelMaskAddr;
+        private readonly int _modelFileUnknownHeaderAddrAddr;
+        private readonly int _modelIndicesAvailableAddr;
 
         public CharacterBattleSc3Model(IByteData data, int id, string name, int address) : base(data, id, name, address, 0x0C) {
-            _modelFileIdAddr       = Address + 0x00; // 4 bytes
-            _unknownFileOffsetAddr = Address + 0x04; // 4 bytes
-            _weaponModelMaskAddr   = Address + 0x08; // 4 bytes
+            _modelFileIdAddr                = Address + 0x00; // 4 bytes
+            _modelFileUnknownHeaderAddrAddr = Address + 0x04; // 4 bytes
+            _modelIndicesAvailableAddr      = Address + 0x08; // 4 bytes
         }
+
+        private bool GetModelIndexAvailable(int promotionWeaponIndex)
+            => ((Data.GetDouble(_modelIndicesAvailableAddr) >> promotionWeaponIndex) & 0x01) == 0x01;
+
+        private void SetModelIndexAvailable(int promotionWeaponIndex, bool value)
+            => Data.SetDouble(_modelIndicesAvailableAddr, value ? (Data.GetDouble(_modelIndicesAvailableAddr) | (0x01 << promotionWeaponIndex)) : (Data.GetDouble(_modelIndicesAvailableAddr) & ~(0x01 << promotionWeaponIndex)));
 
         [TableViewModelColumn(displayOrder: 0, displayFormat: "X3", minWidth: 120)]
         [NameGetter(NamedValueType.FileIndex)]
@@ -24,16 +30,93 @@ namespace SF3.Models.Structs.X014 {
 
         [TableViewModelColumn(displayOrder: 1, displayFormat: "X8", minWidth: 75)]
         [BulkCopy]
-        public uint UnknownFileOffset {
-            get => (uint) Data.GetDouble(_unknownFileOffsetAddr);
-            set => Data.SetDouble(_unknownFileOffsetAddr, (int) value);
+        public uint ModelFileUnknownHeaderAddr {
+            get => (uint) Data.GetDouble(_modelFileUnknownHeaderAddrAddr);
+            set => Data.SetDouble(_modelFileUnknownHeaderAddrAddr, (int) value);
         }
 
-        [TableViewModelColumn(displayOrder: 2, displayFormat: "X8", minWidth: 75)]
+        [TableViewModelColumn(displayOrder: 2)]
         [BulkCopy]
-        public uint WeaponModelMask {
-            get => (uint) Data.GetDouble(_weaponModelMaskAddr);
-            set => Data.SetDouble(_weaponModelMaskAddr, (int) value);
+        public bool UWp1 {
+            get => GetModelIndexAvailable(0);
+            set => SetModelIndexAvailable(0, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 3)]
+        [BulkCopy]
+        public bool UWp2 {
+            get => GetModelIndexAvailable(1);
+            set => SetModelIndexAvailable(1, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 4)]
+        [BulkCopy]
+        public bool UWp3 {
+            get => GetModelIndexAvailable(2);
+            set => SetModelIndexAvailable(2, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 5)]
+        [BulkCopy]
+        public bool UWp4 {
+            get => GetModelIndexAvailable(3);
+            set => SetModelIndexAvailable(3, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 6)]
+        [BulkCopy]
+        public bool P1Wp1 {
+            get => GetModelIndexAvailable(4);
+            set => SetModelIndexAvailable(4, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 7)]
+        [BulkCopy]
+        public bool P1Wp2 {
+            get => GetModelIndexAvailable(5);
+            set => SetModelIndexAvailable(5, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 8)]
+        [BulkCopy]
+        public bool P1Wp3 {
+            get => GetModelIndexAvailable(6);
+            set => SetModelIndexAvailable(6, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 9)]
+        [BulkCopy]
+        public bool P1Wp4 {
+            get => GetModelIndexAvailable(7);
+            set => SetModelIndexAvailable(7, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 10)]
+        [BulkCopy]
+        public bool P2Wp1 {
+            get => GetModelIndexAvailable(8);
+            set => SetModelIndexAvailable(8, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 11)]
+        [BulkCopy]
+        public bool P2Wp2 {
+            get => GetModelIndexAvailable(9);
+            set => SetModelIndexAvailable(9, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 12)]
+        [BulkCopy]
+        public bool P2Wp3 {
+            get => GetModelIndexAvailable(10);
+            set => SetModelIndexAvailable(10, value);
+        }
+
+        [TableViewModelColumn(displayOrder: 13)]
+        [BulkCopy]
+        public bool P2Wp4 {
+            get => GetModelIndexAvailable(11);
+            set => SetModelIndexAvailable(11, value);
         }
     }
 }
