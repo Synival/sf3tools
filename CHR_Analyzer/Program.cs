0d078be8a54f5ddd8aed2d249b135d21ac578f84
Simon Bielman <simon@simonbielman.com>
2025-06-18 00:00:00 +0200

    Initial CHR_Analyzer

---
+++ CHR_Analyzer/Program.cs
@@ -0,0 +1,140 @@
+﻿using CommonLib.Arrays;
+using CommonLib.NamedValues;
+using SF3.ByteData;
+using SF3.Models.Files.CHR;
+using SF3.NamedValues;
+using SF3.Types;
+
+namespace CHR_Analyzer {
+    public class Program {
+        // ,--- Enter the paths for all your CHR files here!
+        // v
+        private static readonly Dictionary<ScenarioType, string> c_pathsIn = new() {
+            { ScenarioType.Scenario1,   "D:/" },
+            { ScenarioType.Scenario2,   "E:/" },
+            { ScenarioType.Scenario3,   "F:/" },
+            { ScenarioType.PremiumDisk, "G:/" },
+        };
+
+        private static List<string> s_matchReports = new List<string>();
+
+        private static bool? CHR_Match_Func(string filename, ICHR_File chrFile) {
+            // TODO: matching!
+            return true;
+        }
+
+        public static void Main(string[] args) {
+            Console.WriteLine("Press a key to start...");
+            _ = Console.ReadKey();
+
+            // Get a list of all .MPD files from all scenarios located at 'c_pathsIn[Scenario]'.
+            var allFiles = Enum.GetValues<ScenarioType>()
+                .Where(x => c_pathsIn.ContainsKey(x))
+                .ToDictionary(x => x, x => {
+                    var files = Directory.GetFiles(c_pathsIn[x], "*.CHR").Order().ToList();
+                    files.AddRange(Directory.GetFiles(c_pathsIn[x], "*.CHP").Order().ToList());
+                    return files;
+                });
+
+            var nameGetterContexts = Enum.GetValues<ScenarioType>()
+                .ToDictionary(x => x, x => (INameGetterContext) new NameGetterContext(x));
+
+            // Open each file.
+            var matchSet   = new List<string>();
+            var nomatchSet = new List<string>();
+
+            foreach (var filesKv in allFiles) {
+                var scenario = filesKv.Key;
+                var nameGetter = nameGetterContexts[scenario];
+
+                foreach (var file in filesKv.Value) {
+                    var filename = Path.GetFileNameWithoutExtension(file);
+
+                    // Get a byte data editing context for the file.
+                    var byteData = new ByteData(new ByteArray(File.ReadAllBytes(file)));
+
+                    // Create an MPD file that works with our new ByteData.
+                    try {
+                        var isBTL99 = filename == "X1BTL99";
+                        using (var chrFile = CHR_File.Create(byteData, nameGetterContexts[scenario], scenario, file.EndsWith(".CHP"))) {
+                            var match = CHR_Match_Func(filename, chrFile);
+
+                            // If the match is 'null', that means we're just skipping this file completely.
+                            if (match == null) {
+                                s_matchReports.Clear();
+                                continue;
+                            }
+
+                            // List the file and any report we may have from CHR_Match_Func().
+                            var fileStr = GetFileString(scenario, file, chrFile);
+                            Console.WriteLine(fileStr + " | ");
+                            foreach (var mr in s_matchReports)
+                                Console.WriteLine("    " + mr);
+                            s_matchReports.Clear();
+
+                            if (match == true)
+                                matchSet.Add(fileStr);
+                            else
+                                nomatchSet.Add(fileStr);
+
+                            ScanForErrorsAndReport(scenario, chrFile);
+                        }
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine("  !!! Exception for '" + filename + "': '" + e.Message + "'. Skipping!");
+                    }
+                }
+            }
+
+            var totalCount = matchSet.Count + nomatchSet.Count;
+
+            Console.WriteLine("");
+            Console.WriteLine("===================================================");
+            Console.WriteLine("| MATCH RESULTS                                   |");
+            Console.WriteLine("===================================================");
+
+            Console.WriteLine("");
+            Console.WriteLine($"Match: {matchSet.Count}/{totalCount}");
+            foreach (var str in matchSet)
+                Console.WriteLine("  " + str);
+
+            Console.WriteLine($"NoMatch: {nomatchSet.Count}/{totalCount}");
+            foreach (var str in nomatchSet)
+                Console.WriteLine("  " + str);
+        }
+
+        private static string BitString(uint bits) {
+            var str = "";
+            for (var i = 0; i < 32; i++) {
+                if (i % 4 == 0 && i != 0)
+                    str += ",";
+                str += (bits & (0x8000_0000 >> i)) != 0 ? "1" : "0";
+            }
+            return str;
+        }
+
+        private static string BitString(ushort bits) {
+            var str = "";
+            for (var i = 0; i < 16; i++) {
+                if (i % 4 == 0 && i != 0)
+                    str += ",";
+                str += (bits & (0x8000 >> i)) != 0 ? "1" : "0";
+            }
+            return str;
+        }
+
+        private static string GetFileString(ScenarioType inputScenario, string filename, ICHR_File chrFile) {
+            var typeStr = chrFile.IsCHP ? "CHP" : "CHR";
+            return inputScenario.ToString().PadLeft(11) + ": " + Path.GetFileName(filename).PadLeft(12) + " | " + typeStr;
+        }
+
+        private static void ScanForErrorsAndReport(ScenarioType inputScenario, ICHR_File mpdFile) {
+            var totalErrors = new List<string>();
+
+            // TODO: scan for errors
+
+            foreach (var error in totalErrors)
+                Console.WriteLine("    !!! " + error);
+        }
+    }
+}
