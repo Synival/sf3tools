c6bf031ef499e1d043891664153958eafc2e2887
Simon Bielman <simon@simonbielman.com>
2025-07-16 00:00:00 +0200

    Removed unused AnimationDef.Hash property

--- SF3Lib/Sprites/AnimationDef.cs
+++ SF3Lib/Sprites/AnimationDef.cs
@@ -5,13 +5,11 @@ namespace SF3.Sprites {
         public AnimationDef() { }
 
         public AnimationDef(UniqueAnimationDef aniInfo) : this() {
-            Hash = aniInfo.AnimationHash;
             AnimationFrames = aniInfo.AnimationFrames;
         }
 
         public override string ToString() => string.Join(", ", AnimationFrames.Select(x => x.ToString()));
 
-        public string Hash;
         public AnimationFrameDef[] AnimationFrames;
     };
 }
