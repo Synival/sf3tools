571df9342173ccb0f885783879abb0357328e9d6
Simon Bielman <simon@simonbielman.com>
2025-07-16 00:00:00 +0200

    Removed AnimationDef.Name

--- SF3Lib/Sprites/AnimationDef.cs
+++ SF3Lib/Sprites/AnimationDef.cs
@@ -1,16 +1,16 @@
-﻿namespace SF3.Sprites {
+﻿using System.Linq;
+
+namespace SF3.Sprites {
     public class AnimationDef {
         public AnimationDef() { }
 
         public AnimationDef(UniqueAnimationDef aniInfo) : this() {
-            Name = aniInfo.AnimationName;
             Hash = aniInfo.AnimationHash;
             AnimationFrames = aniInfo.AnimationFrames;
         }
 
-        public override string ToString() => Name;
+        public override string ToString() => string.Join(", ", AnimationFrames.Select(x => x.ToString()));
 
-        public string Name;
         public string Hash;
         public AnimationFrameDef[] AnimationFrames;
     };
