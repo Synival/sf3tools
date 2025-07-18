d9e2dd8682d6418ab5b8b41977a2b3610c66c2d7
Simon Bielman <simon@simonbielman.com>
2025-07-19 00:00:00 +0200

    Placeholder CHP/CHR deserialization

--- SF3Lib/Sprites/CHP_Def.cs
+++ SF3Lib/Sprites/CHP_Def.cs
@@ -1,7 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using SF3.Models.Files.CHP;
 
 namespace SF3.Sprites {
     public class CHP_Def {
         public Dictionary<int, CHR_Def> CHRsByOffset;
+
+        public ICHP_File ToCHP_File() {
+            // TODO: deserialize!
+            return null;
+        }
     }
 }
--- SF3Lib/Sprites/CHR_Def.cs
+++ SF3Lib/Sprites/CHR_Def.cs
@@ -1,5 +1,13 @@
-﻿namespace SF3.Sprites {
+﻿using System;
+using SF3.Models.Files.CHR;
+
+namespace SF3.Sprites {
     public class CHR_Def {
         public CHR_SpriteDef[] Sprites;
+
+        public ICHR_File ToCHR_File() {
+            // TODO: deserialize!
+            return null;
+        }
     }
 }
