5dfad678b66072c3b7fc1f24532e6b21f43ba79a
Simon Bielman <simon@simonbielman.com>
2025-07-21 00:00:00 +0200

    Proper tests for actual cases

--- SF3LibTests/CHR/CHR_DefTests.cs
+++ SF3LibTests/CHR/CHR_DefTests.cs
@@ -1,9 +1,15 @@
 ﻿using Newtonsoft.Json;
 using SF3.CHR;
+using SF3.NamedValues;
+using SF3.Types;
 
 namespace SF3.Tests.CHR {
     [TestClass]
     public class CHR_DefTests {
+        private static readonly string c_emptyCHR_Text = @"
+            { 'Sprites': [] }
+        ";
+
         private static readonly string c_minimalCHR_Text = @"
             { 'Sprites': [
                 {
@@ -29,10 +35,11 @@ namespace SF3.Tests.CHR {
             ]}
         ";
 
+        private static CHR_Def c_emptyCHR = JsonConvert.DeserializeObject<CHR_Def>(c_emptyCHR_Text)!;
         private static CHR_Def c_minimalCHR = JsonConvert.DeserializeObject<CHR_Def>(c_minimalCHR_Text)!;
 
         [TestMethod]
-        public void ToCHR_File_WithMinimalCHR_ExportsExpectedData() {
+        public void ToCHR_File_ToStream_WithEmptyCHR_ExportsExpectedData() {
             // TODO: get real data!
             var expectedData = new byte[] {
                 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
@@ -43,12 +50,19 @@ namespace SF3.Tests.CHR {
             byte[]? resultData = null;
             int rval;
             using (var outputStream = new MemoryStream()) {
-                rval = c_minimalCHR.ToCHR_File(outputStream);
+                rval = c_emptyCHR.ToCHR_File(outputStream);
                 resultData = outputStream.ToArray();
             }
 
             Assert.IsTrue(Enumerable.SequenceEqual(expectedData, resultData));
             Assert.AreEqual(expectedData.Length, rval);
+        }
+
+        [TestMethod]
+        public void ToCHR_File_ToFile_WithEmptyCHR_ExportsExpectedData() {
+            var nameGetterContext = new NameGetterContext(ScenarioType.Scenario1);
+            var chrFile = c_emptyCHR.ToCHR_File(nameGetterContext, nameGetterContext.Scenario);
+            Assert.AreEqual(0, chrFile.SpriteTable.Length);
         }
     }
 }
