28c790b377d28d35db068f4f79aaafd11cb9e997
Simon Bielman <simon@simonbielman.com>
2025-11-08 00:00:00 +0100

    Crude palette export+import

--- SF3LibWin/Views/ColorTableView.cs
+++ SF3LibWin/Views/ColorTableView.cs
@@ -1,19 +1,24 @@
 using System;
 using System.Drawing;
-using System.Drawing.Imaging;
+using System.Linq;
 using System.Windows.Forms;
 using BrightIdeasSoftware;
-using CommonLib.Imaging;
 using CommonLib.NamedValues;
 using SF3.Models.Tables.MPD;
-using SF3.Win.Controls;
 
 namespace SF3.Win.Views {
     public class ColorTableView : ControlSpaceView {
         public ColorTableView(string name, ColorTable table, INameGetterContext nameGetterContext) : base(name) {
-            Table       = table;
-            TableView   = new TableView("Table", table, nameGetterContext);
-            ImageView = new ImageView("Texture");
+            Table     = table;
+            TableView = new TableView("Table", table, nameGetterContext);
+            PaletteView = new PaletteView("Texture");
+
+            PaletteView.ImportPalette += (s, colors) => {
+                int max = Math.Min(Table.Length, colors.Length);
+                for (int i = 0; i < max; i++)
+                    Table[i].ColorABGR1555 = colors[i];
+                UpdateBitmap();
+            };
         }
 
         public override Control Create() {
@@ -30,7 +35,7 @@ namespace SF3.Win.Views {
                 };
             });
 
-            CreateChild(ImageView, (c) => {
+            CreateChild(PaletteView, (c) => {
                 c.Dock = DockStyle.Right;
                 UpdateBitmap();
             }, false);
@@ -39,45 +44,15 @@ namespace SF3.Win.Views {
         }
 
         private void UpdateBitmap() {
-            var colorCount = Table.Length;
-            var width = (int) Math.Ceiling(Math.Sqrt(colorCount));
-
-            var nextPow = 1;
-            while (width > nextPow)
-                nextPow *= 2;
-            width = nextPow;
-
-            var height = (int) Math.Ceiling(colorCount / (float) width);
-
-            PaletteBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-            int y = 0, x = 0;
-            using (var g = Graphics.FromImage(PaletteBitmap)) {
-                g.Clear(Color.Transparent);
-                for (var i = 0; i < colorCount; i++) {
-                    var colorChannels = PixelConversion.ABGR1555toChannels(Table[i].ColorABGR1555);
-                    colorChannels.a = 0xff;
-                    var brush = new SolidBrush(Color.FromArgb(colorChannels.a, colorChannels.r, colorChannels.g, colorChannels.b));
-                    g.FillRectangle(brush, x, y, 1, 1);
-
-                    if (++x == width) {
-                        x = 0;
-                        ++y;
-                    }
-                }
-            }
-
-            var ic = ImageView?.Control;
-            if (ic != null) {
-                ic.ImageScale = (int) Math.Ceiling(128.0f / PaletteBitmap.Width);
-                ic.Image = PaletteBitmap;
-            }
+            var colors = Table.Select(x => x.ColorABGR1555).ToArray();
+            PaletteView.SetColors(colors);
         }
 
         public readonly ColorTable Table = null;
         public readonly TableView TableView = null;
-        public readonly ImageView ImageView = null;
+        public readonly PaletteView PaletteView = null;
 
-        public Bitmap PaletteBitmap { get; private set; } = null;
+        public Bitmap PaletteBitmap => PaletteView.PaletteBitmap;
 
     }
 }
---
+++ SF3LibWin/Views/PaletteView.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Forms;
+using CommonLib.Imaging;
+
+namespace SF3.Win.Views {
+    public class PaletteView : ImageView {
+        public PaletteView(string name, float? imageScale = null) : base(name, imageScale) {
+        }
+
+        public void SetColors(ushort[] colors) {
+            _colors = colors;
+
+            if (colors == null) {
+                PaletteBitmap = null;
+                Image = null;
+                if (Control != null) {
+                    Control.ExportAction = null;
+                    Control.ImportAction = null;
+                }
+                return;
+            }
+
+            var width = (int) Math.Ceiling(Math.Sqrt(colors.Length));
+
+            var nextPow = 1;
+            while (width > nextPow)
+                nextPow *= 2;
+            width = nextPow;
+
+            var height = (int) Math.Ceiling(colors.Length / (float) width);
+
+            PaletteBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            int y = 0, x = 0;
+            using (var g = Graphics.FromImage(PaletteBitmap)) {
+                g.Clear(Color.Transparent);
+                for (var i = 0; i < colors.Length; i++) {
+                    var colorChannels = PixelConversion.ABGR1555toChannels(colors[i]);
+                    colorChannels.a = 0xff;
+                    var brush = new SolidBrush(Color.FromArgb(colorChannels.a, colorChannels.r, colorChannels.g, colorChannels.b));
+                    g.FillRectangle(brush, x, y, 1, 1);
+
+                    if (++x == width) {
+                        x = 0;
+                        ++y;
+                    }
+                }
+            }
+
+            if (Control != null) {
+                Control.ImageScale = (int) Math.Ceiling(128.0f / PaletteBitmap.Width);
+                Control.Image = PaletteBitmap;
+                Control.ExportAction = ExportPaletteDialog;
+                Control.ImportAction = ImportPaletteDialog;
+            }
+        }
+
+        void ExportPaletteDialog() {
+            if (_colors == null)
+                return;
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Palettes|*.sf3pal";
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                var content = JsonSerializer.Serialize(_colors, new JsonSerializerOptions() { WriteIndented = true });
+                File.WriteAllText(dialog.FileName, content);
+            }
+        }
+
+        void ImportPaletteDialog() {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Palettes|*.sf3pal";
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                var text = File.ReadAllText(dialog.FileName);
+                var colors = JsonSerializer.Deserialize<ushort[]>(text);
+                ImportPalette?.Invoke(this, colors);
+            }
+        }
+
+        private ushort[] _colors = null;
+        public Bitmap PaletteBitmap { get; private set; } = null;
+
+        public delegate void ImportPaletteEventHandler(object source, ushort[] colors);
+        public event ImportPaletteEventHandler ImportPalette;
+    }
+}
