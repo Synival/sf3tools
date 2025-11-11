d870561ec9c41222ddfa7b87b483252ed23372f9
Simon Bielman <simon@simonbielman.com>
2025-11-12 00:00:00 +0100

    Added documentation

--- SF3LibWin/Extensions/ControlExtensions.cs
+++ SF3LibWin/Extensions/ControlExtensions.cs
@@ -2,6 +2,12 @@
 
 namespace SF3.Win.Extensions {
     public static class ControlExtensions {
+        /// <summary>
+        /// Gets the bottom-most control that has user focus by checking ActiveControl and recrusing down.
+        /// Returns 'null' if 'ContainsFocus' is 'false'.
+        /// </summary>
+        /// <param name="control">The control the check for focus.</param>
+        /// <returns>A Control that has no 'ActiveControl' child set but is active itself.</returns>
         public static Control GetFocusedControl(this Control control) {
             if (!control.ContainsFocus)
                 return null;
