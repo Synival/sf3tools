62b5d26a2fd6e00026e9bbac2acd9459fb8e3b4c
Simon Bielman <simon@simonbielman.com>
2025-11-14 00:00:00 +0100

    Added DarkModeLabel

---
+++ SF3LibWin/Controls/DarkModeLabel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using SF3.Win.DarkMode;
+
+namespace SF3.Win.Controls {
+    public class DarkModeLabel : Label {
+        protected override void OnHandleCreated(EventArgs e) {
+            base.OnHandleCreated(e);
+            if (DarkModeContext == null) {
+                DarkModeContext = new DarkModeControlContext<DarkModeLabel>(this);
+                DarkModeContext.EnabledChanged += (s, e) => {
+                    DisabledColor = DarkModeContext.Enabled ? DarkModeColors.DisabledColor : Color.Empty;
+                };
+                DarkModeContext.Init();
+                DarkModeContext.OriginalBackColor = DefaultBackColor;
+            }
+        }
+
+        private static TextFormatFlags ToTextFormatFlags(ContentAlignment align) {
+            switch (align) {
+                case ContentAlignment.TopLeft:      return TextFormatFlags.Top            | TextFormatFlags.Left;
+                case ContentAlignment.TopCenter:    return TextFormatFlags.Top            | TextFormatFlags.HorizontalCenter;
+                case ContentAlignment.TopRight:     return TextFormatFlags.Top            | TextFormatFlags.Right;
+                case ContentAlignment.MiddleLeft:   return TextFormatFlags.VerticalCenter | TextFormatFlags.Left;
+                case ContentAlignment.MiddleCenter: return TextFormatFlags.VerticalCenter | TextFormatFlags.HorizontalCenter;
+                case ContentAlignment.MiddleRight:  return TextFormatFlags.VerticalCenter | TextFormatFlags.Right;
+                case ContentAlignment.BottomLeft:   return TextFormatFlags.Bottom         | TextFormatFlags.Left;
+                case ContentAlignment.BottomCenter: return TextFormatFlags.Bottom         | TextFormatFlags.HorizontalCenter;
+                case ContentAlignment.BottomRight:  return TextFormatFlags.Bottom         | TextFormatFlags.Right;
+                default: return TextFormatFlags.Default;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            var textFormatFlags = ToTextFormatFlags(TextAlign);
+            if (!Enabled && DisabledColor != Color.Empty)
+                TextRenderer.DrawText(e.Graphics, Text, Font, ClientRectangle, DisabledColor, textFormatFlags);
+            else
+                base.OnPaint(e);
+        }
+
+        public Color DisabledColor { get; set; } = Color.Empty;
+        private DarkModeControlContext<DarkModeLabel> DarkModeContext { get; set; }
+    }
+}
