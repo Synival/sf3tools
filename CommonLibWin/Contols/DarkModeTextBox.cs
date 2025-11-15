b9b83504e8c25713ffecbf15d14230cfe9897524
Simon Bielman <simon@simonbielman.com>
2025-11-16 00:00:00 +0100

    DarkModeTextBox borders look nicer in dark mode

--- CommonLibWin/Contols/DarkModeTextBox.cs
+++ CommonLibWin/Contols/DarkModeTextBox.cs
@@ -7,18 +7,26 @@ namespace CommonLib.Win.Controls {
     /// TextBox with dark mode support.
     /// </summary>
     public class DarkModeTextBox : TextBox {
-        public DarkModeTextBox() {
-            BorderStyle = BorderStyle.FixedSingle;
-        }
-
         protected override void OnHandleCreated(EventArgs e) {
             base.OnHandleCreated(e);
-            if (DarkModeContext == null) {
+            if (DarkModeContext == null) {                
                 DarkModeContext = new DarkModeControlContext<DarkModeTextBox>(this);
+
+                OriginalBorderStyle = BorderStyle;
+                if (OriginalBorderStyle != BorderStyle.None && BorderStyle != BorderStyle.FixedSingle) {
+                    DarkModeContext.EnabledChanged += (s, e) => {
+                        if (DarkModeContext.Enabled)
+                            BeginInvoke(() => this.BorderStyle = BorderStyle.FixedSingle);
+                        else
+                            BeginInvoke(() => this.BorderStyle = OriginalBorderStyle);
+                    };
+                }
+
                 DarkModeContext.Init();
             }
         }
 
         private DarkModeControlContext<DarkModeTextBox> DarkModeContext { get; set; }
+        public BorderStyle OriginalBorderStyle { get; private set; }
     }
 }
