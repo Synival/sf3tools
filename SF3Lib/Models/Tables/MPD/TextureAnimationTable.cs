72d7f4abcce205f9f7a6fecc03afb8ca02e3acc8
Simon Bielman <simon@simonbielman.com>
2025-11-18 00:00:00 +0100

    Exposed TextureAnimationTable

--- SF3Lib/Models/Tables/MPD/TextureAnimationTable.cs
+++ SF3Lib/Models/Tables/MPD/TextureAnimationTable.cs
@@ -4,9 +4,9 @@ using SF3.Models.Structs.MPD;
 namespace SF3.Models.Tables.MPD {
     public class TextureAnimationTable : TerminatedTable<TextureAnimationModel> {
         protected TextureAnimationTable(IByteData data, string name, int address, bool is32Bit) : base(data, name, address, is32Bit ? 8 : 4, null) {
-            Is32Bit       = is32Bit;
-            _frameEndId   = is32Bit ? 0xFFFF_FFFE : 0xFFFE;
-            _textureEndId = is32Bit ? 0xFFFF_FFFF : 0xFFFF;
+            Is32Bit      = is32Bit;
+            FrameEndId   = is32Bit ? 0xFFFF_FFFE : 0xFFFE;
+            TextureEndId = is32Bit ? 0xFFFF_FFFF : 0xFFFF;
         }
 
         public static TextureAnimationTable Create(IByteData data, string name, int address, bool is32Bit)
@@ -18,15 +18,14 @@ namespace SF3.Models.Tables.MPD {
                     // For some reason, Scenario 2's SARA23.MPD's final animation has an ID of 0xFFFE instead of 0xFFFF like
                     // everything else. No clue why, but let's consider that the end as well.
                     var textureId = Data.GetData(address, Is32Bit ? 4 : 2);
-                    var atEnd = textureId == _frameEndId || textureId == _textureEndId;
+                    var atEnd = textureId == FrameEndId || textureId == TextureEndId;
                     return new TextureAnimationModel(Data, id, atEnd ? "--" : "TexAnim" + id, address, Is32Bit);
                 },
-                (currentRows, model) => model.TextureID != _frameEndId && model.TextureID != _textureEndId, addEndModel: false);
+                (currentRows, model) => model.TextureID != FrameEndId && model.TextureID != TextureEndId, addEndModel: false);
         }
 
         public bool Is32Bit { get; }
-
-        private readonly uint _frameEndId;
-        private readonly uint _textureEndId;
+        public uint FrameEndId { get; }
+        public uint TextureEndId { get; }
     }
 }
