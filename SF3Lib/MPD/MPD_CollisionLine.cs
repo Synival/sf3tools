539c8f389d997548f551f11d428f3c61ffb607fb
Simon Bielman <simon@simonbielman.com>
2025-12-12 00:00:00 +0100

    Added concrete MPD_CollisionLine and MPD_CollisionPoint

---
+++ SF3Lib/MPD/MPD_CollisionLine.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace SF3.MPD {
+    /// <summary>
+    /// A single line connected by two MPD_CollisionPoint's in an MPD.
+    /// </summary>
+    public class MPD_CollisionLine : IMPD_CollisionLine {
+        public MPD_CollisionLine(IMPD_CollisionPoint point1, IMPD_CollisionPoint point2) {
+            // TODO: Enforce non-null assignment.
+            Point1 = point1;
+            Point2 = point2;
+        }
+
+        // TODO: Enforce non-null assignment.
+        public IMPD_CollisionPoint Point1 { get; set; }
+        // TODO: Enforce non-null assignment.
+        public IMPD_CollisionPoint Point2 { get; set; }
+
+        public short X1 {
+            get => Point1?.X ?? 0;
+            set {
+                var point = Point1;
+                if (point != null)
+                    point.X = value;
+            }
+        }
+
+        public short Y1 {
+            get => Point1?.Y ?? 0;
+            set {
+                var point = Point1;
+                if (point != null)
+                    point.Y = value;
+            }
+        }
+
+        public short X2 {
+            get => Point2?.X ?? 0;
+            set {
+                var point = Point2;
+                if (point != null)
+                    point.X = value;
+            }
+        }
+
+        public short Y2 {
+            get => Point2?.Y ?? 0;
+            set {
+                var point = Point2;
+                if (point != null)
+                    point.Y = value;
+            }
+        }
+
+        // TODO: Calculate angle properly
+        public float Angle => 0.00f;
+
+        // TODO: Restict between 0x201 and 0x2FF (inclusive).
+        public int? FlagToDisable { get; set; }
+
+        public byte Tag { get; set; }
+
+        // TODO: Actually calculate this!
+        public IEnumerable<(int X, int Y)> GetReferencingBlocks() => new (int X, int Y)[0];
+    }
+}
---
+++ SF3Lib/MPD/MPD_CollisionPoint.cs
@@ -0,0 +1,14 @@
+﻿namespace SF3.MPD {
+    /// <summary>
+    /// A single point from which MPD collision lines can be connected.
+    /// </summary>
+    public class MPD_CollisionPoint : IMPD_CollisionPoint {
+        public MPD_CollisionPoint(short x, short y) {
+            X = x;
+            Y = y;
+        }
+
+        public short X { get; set; }
+        public short Y { get; set; }
+    }
+}
