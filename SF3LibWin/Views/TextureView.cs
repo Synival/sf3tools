486bc480d5e6c779fe694cfdd6e30b54937f783f
Simon Bielman <simon@simonbielman.com>
2025-12-13 00:00:00 +0100

    Reimplement TextureView to assign Import button behavior itself based on ITextureData content

--- SF3LibWin/Views/TextureView.cs
+++ SF3LibWin/Views/TextureView.cs
@@ -1,6 +1,11 @@
-﻿using System.Drawing.Imaging;
-using SF3.Images;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using CommonLib.Logging;
+using CommonLib.Win.Utils;
+using CommonLib.Extensions;
 using SF3.Win.Extensions;
+using SF3.Images;
 
 namespace SF3.Win.Views {
     public class TextureView : ImageView {
@@ -20,8 +25,77 @@ namespace SF3.Win.Views {
                 if (value != _texture) {
                     _texture = value;
                     Image = value?.CreateBitmap(AppState.RetrieveAppState().HighlightEndCodesInTextureView);
+
+                    if (_texture != null && (_texture.CanSetImageData8Bit || _texture.CanSetImageData16Bit)) {
+                        Control.ImportAction = ImportImageDialog;
+                        LoadImageAction = LoadImage;
+                    }
+                    else {
+                        Control.ImportAction = null;
+                        LoadImageAction = null;
+                    }
                 }
             }
+        }
+
+        private void LoadImage(Image image, string filename) {
+            if (image == null)
+                return;
+
+            var canReplaceTexture8Bit  = _texture.CanSetImageData8Bit;
+            var canReplaceTexture16Bit = _texture.CanSetImageData16Bit;
+
+            if (image.PixelFormat == PixelFormat.Format8bppIndexed && canReplaceTexture8Bit) {
+                var bitmap = image.CreateIndexedBitmap();
+                var data = bitmap.Get2DDataIndexed();
+                var palette = bitmap.GetPalette();
+                var validatorResult = _texture.Validate8BitImageData(data, palette);
+
+                if (validatorResult == null) {
+                    try {
+                        _texture.SetImageData8Bit(data, palette);
+                    }
+                    catch (Exception e) {
+                        Logger.LogException(e);
+                        validatorResult = e.Message;
+                    }
+                }
+
+                if (validatorResult != null) {
+                    MessageUtils.ErrorMessage(validatorResult);
+                    return;
+                }
+            }
+            else if (canReplaceTexture16Bit) {
+                var data = image.Get2DDataABGR1555();
+                var validatorResult = _texture.Validate16BitImageData(data);
+
+                if (validatorResult == null) {
+                    try {
+                        _texture.ImageData16Bit = data;
+                    }
+                    catch (Exception e) {
+                        Logger.LogException(e);
+                        validatorResult = e.Message;
+                    }
+                }
+
+                if (validatorResult != null) {
+                    MessageUtils.ErrorMessage(validatorResult);
+                    return;
+                }
+            }
+            else {
+                if (canReplaceTexture16Bit)
+                    MessageUtils.ErrorMessage("Image is not compatible with 16-bit ABGR format");
+                else if (canReplaceTexture8Bit)
+                    MessageUtils.ErrorMessage("Image must be in 8-bit indexed format");
+                else
+                    MessageUtils.ErrorMessage("Image format is invalid");
+                return;
+            }
+
+            Image = _texture?.CreateBitmap(AppState.RetrieveAppState().HighlightEndCodesInTextureView);
         }
     }
 }
