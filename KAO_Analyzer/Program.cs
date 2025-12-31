60d993e9694eb52dfc09f7e60f27b928ee264003
Simon Bielman <simon@simonbielman.com>
2026-01-01 00:00:00 +0100

    Added KAO_Analyzer

---
+++ KAO_Analyzer/Program.cs
@@ -0,0 +1,114 @@
+﻿using CommonLib.Arrays;
+using CommonLib.Extensions;
+using CommonLib.NamedValues;
+using SF3.ByteData;
+using SF3.Models.Files.KAO;
+using SF3.NamedValues;
+using SF3.Types;
+
+namespace MPD_Analyzer {
+    public class Program {
+        // ,--- Enter the paths for all your MPD files here!
+        // v
+        private static readonly Dictionary<ScenarioType, string> c_pathsIn = new() {
+            { ScenarioType.Scenario1,   "D:/" },
+            { ScenarioType.Scenario2,   "E:/" },
+            { ScenarioType.Scenario3,   "F:/" },
+            { ScenarioType.PremiumDisk, "G:/" },
+        };
+
+        private static string[]? KAO_MatchFunc(IKAO_File kaoFile, string filename) {
+            var matches = new List<string>();
+            foreach (var face in kaoFile.FaceChunkTable) {
+                var baseImageData = face.ImageTable[0].ImageData8Bit;
+                var zeroCount = baseImageData.To1DArray().Count(x => x == 0);
+                if (zeroCount > 0)
+                    matches.Add($"Face[{face.ID}]: {zeroCount} zero pixel(s)");
+            }
+            return matches.ToArray();
+        }
+
+        public static void Main(string[] args) {
+            // Get a list of all .MPD files from all scenarios located at 'c_pathsIn[Scenario]'.
+            var allFiles = Enum.GetValues<ScenarioType>()
+                .Where(x => c_pathsIn.ContainsKey(x))
+                .ToDictionary(x => x, x => Directory.GetFiles(c_pathsIn[x], "KAO*.DAT").Order().ToList());
+            var nameGetterContexts = Enum.GetValues<ScenarioType>()
+                .ToDictionary(x => x, x => (INameGetterContext) new NameGetterContext(x));
+
+            // Sets of matching/unmatching KAO files.
+            var matchSet   = new List<string>();
+            var nomatchSet = new List<string>();
+
+            // Open each file.
+            foreach (var filesKv in allFiles) {
+                var scenario = filesKv.Key;
+                var nameGetter = nameGetterContexts[scenario];
+
+                foreach (var file in filesKv.Value) {
+                    var filename = Path.GetFileNameWithoutExtension(file);
+
+                    // Get a byte data editing context for the file.
+                    var byteData = new ByteData(new ByteArray(File.ReadAllBytes(file)));
+
+                    // Create an MPD file that works with our new ByteData.
+                    try {
+                        using (var kaoFile = KAO_File.Create(byteData, nameGetter, scenario)) {
+                            // Condition for match checks here
+                            var matchReports = KAO_MatchFunc(kaoFile, filename);
+                            if (matchReports == null)
+                                continue;
+
+                            bool match = matchReports.Length > 0;
+                            var fileStr = GetFileString(scenario, file, kaoFile);
+                            Console.WriteLine(fileStr + " | " + (match ? "Match  " : "NoMatch"));
+                            if (matchReports.Length > 0) {
+                                foreach (var r in matchReports)
+                                    Console.WriteLine("    " + filename.PadLeft(8) + " | " + r);
+                                Console.WriteLine();
+                            }
+
+                            if (match)
+                                matchSet.Add(fileStr);
+                            else
+                                nomatchSet.Add(fileStr);
+
+                            ScanForErrorsAndReport(scenario, kaoFile);
+                        }
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine("  !!! Exception for '" + filename + "': '" + e.Message + "'. Skipping!");
+                    }
+                }
+            }
+
+            var totalCount = matchSet.Count + nomatchSet.Count;
+
+            Console.WriteLine("");
+            Console.WriteLine("===================================================");
+            Console.WriteLine("| MATCH RESULTS                                   |");
+            Console.WriteLine("===================================================");
+
+            Console.WriteLine("");
+            Console.WriteLine($"Match: {matchSet.Count}/{totalCount}");
+            foreach (var str in matchSet)
+                Console.WriteLine("  " + str);
+
+            Console.WriteLine($"NoMatch: {nomatchSet.Count}/{totalCount}");
+            foreach (var str in nomatchSet)
+                Console.WriteLine("  " + str);
+        }
+
+        private static string GetFileString(ScenarioType inputScenario, string filename, KAO_File kaoFile) {
+            return inputScenario.ToString().PadLeft(11) + ": " + Path.GetFileName(filename).PadLeft(12)
+                + " | " + kaoFile.FaceChunkTable.Length + " face(s)";
+        }
+
+        private static void ScanForErrorsAndReport(ScenarioType inputScenario, KAO_File kaoFile) {
+            var totalErrors = new List<string>();
+            totalErrors.AddRange(kaoFile.GetErrors());
+            foreach (var error in totalErrors)
+                Console.WriteLine("    !!! " + error);
+        }
+    }
+}
