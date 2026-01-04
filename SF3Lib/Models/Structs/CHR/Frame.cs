ae74f04f5816c66372f16012d61f959aecfae964
Simon Bielman <simon@simonbielman.com>
2026-01-05 00:00:00 +0100

    Replaced old TextureABGR1555 with TextureData

--- SF3Lib/Models/Structs/CHR/Frame.cs
+++ SF3Lib/Models/Structs/CHR/Frame.cs
@@ -34,12 +34,12 @@ namespace SF3.Models.Structs.CHR {
 
             if (TextureOffset != 0) {
                 var texData = GetUncompressedTextureData();
-                Texture = new TextureABGR1555(0, 0, 0, 0, texData);
+                Texture = new TextureData(texData, null, zeroIsTransparent: false, canSetImage: false);
                 var hash = Texture.Hash.ToLower();
 
                 // There's one specific Tybalt sprite with some problems.
                 if (ErrorWhileDecompressing != null && s_brokenTybaltHashes.Contains(hash)) {
-                    Texture = new TextureABGR1555(0, 0, 0, 0, texData.To1DArrayTransposed().To2DArrayColumnMajor(48, 40));
+                    Texture = new TextureData(texData.To1DArrayTransposed().To2DArrayColumnMajor(48, 40), null, zeroIsTransparent: false, canSetImage: false);
                     Width  = Texture.Width;
                     Height = Texture.Height;
                 }
--- SF3Lib/Sprites/SpriteResources.cs
+++ SF3Lib/Sprites/SpriteResources.cs
@@ -259,7 +259,7 @@ namespace SF3.Sprites {
                                 if (frame.Coding != SpriteImageCodingType.Ignore)
                                     bitmapData.FixSaturnTransparency(frame.Coding == SpriteImageCodingType.On);
 
-                                var texture = new TextureABGR1555(0, 0, 0, 0, bitmapData);
+                                var texture = new TextureData(bitmapData, null, zeroIsTransparent: false, canSetImage: false);
                                 var hash = texture.Hash;
                                 if (AddFrameRef(hash, spriteDef.Name, frameSize.Width, frameSize.Height, frameGroupName, frameDir))
                                     framesAdded++;
