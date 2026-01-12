68b9c3894381ab0b84a57ef32a7f93197c3e28cc
Simon Bielman <simon@simonbielman.com>
2026-01-13 00:00:00 +0100

    Remove ISGL_Model.ID setter (not necessarilly possible to set)

--- CommonLib/SGL/ISGL_Model.cs
+++ CommonLib/SGL/ISGL_Model.cs
@@ -1,6 +1,6 @@
 ﻿namespace CommonLib.SGL {
     public interface ISGL_Model {
-        int ID { get; set; }
+        int ID { get; }
         IIndexedEnumerableWithLength<VECTOR> Vertices { get; }
         IIndexedEnumerableWithLength<ISGL_ModelFace> Faces { get; }
     }
