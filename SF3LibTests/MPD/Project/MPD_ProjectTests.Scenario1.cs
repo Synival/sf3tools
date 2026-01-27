38d0ced11957566497c86a69d6e26272b87fd856
Simon Bielman <simon@simonbielman.com>
2026-01-28 00:00:00 +0100

    Added other passing tests from MPD_WriterTests.Scenario1 to MPD_ProjectTests.Scenario1

--- SF3LibTests/MPD/Project/MPD_ProjectTests.Scenario1.cs
+++ SF3LibTests/MPD/Project/MPD_ProjectTests.Scenario1.cs
@@ -26,5 +26,72 @@ namespace SF3.Tests.MPD.Project {
         public void Copy_WithScenario1_HONJIN_ProducesSameMPDAsOriginal()
             => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "HONJIN");
 
+        [TestMethod]
+        public void Copy_WithScenario1_BAL_3_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "BAL_3");
+/*
+        [TestMethod]
+        public void Copy_WithScenario1_BALSA_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "BALSA");
+*/
+        [TestMethod]
+        public void Copy_WithScenario1_DAM_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "DAM");
+/*
+        [TestMethod]
+        public void Copy_WithScenario1_MUCHUR_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "MUCHUR");
+
+        [TestMethod]
+        public void Copy_WithScenario1_BTL03_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "BTL03");
+
+        [TestMethod]
+        public void Copy_WithScenario1_BTL02_CanBeLoaded()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "BTL02");
+
+        [TestMethod]
+        public void Copy_WithScenario1_Z_AS_ProducesSameData()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "Z_AS");
+*/
+        [TestMethod]
+        public void Copy_WithScenario1_CHOU00_ProducesSameData()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "CHOU00");
+
+        [TestMethod]
+        public void Copy_WithScenario1_GDI_ProducesSameData()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "GDI");
+
+        [TestMethod]
+        public void Copy_WithScenario1_MGMA00_ProducesSameData()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "MGMA00");
+
+        [TestMethod]
+        public void Copy_WithScenario1_MGMA01_ProducesSameData()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "MGMA01");
+
+        [TestMethod]
+        public void Copy_WithScenario1_JOUSAI_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "JOUSAI");
+/*
+        [TestMethod]
+        public void Copy_WithScenario1_YAKA3_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "YAKA3");
+
+        [TestMethod]
+        public void Copy_WithScenario1_SARA03_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "SARA03");
+
+        [TestMethod]
+        public void Copy_WithScenario1_SARA04_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "SARA04");
+
+        [TestMethod]
+        public void Copy_WithScenario1_FED06_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "FED06");
+*/
+        [TestMethod]
+        public void Copy_WithScenario1_BAKA2_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.Scenario1, "BAKA2");
     }
 }
