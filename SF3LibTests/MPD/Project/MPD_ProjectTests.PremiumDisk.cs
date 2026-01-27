08aef76748eef5b036220b5cfdb3751a7803e7be
Simon Bielman <simon@simonbielman.com>
2026-01-28 00:00:00 +0100

    Added MPD_Project premium disk tests

---
+++ SF3LibTests/MPD/Project/MPD_ProjectTests.PremiumDisk.cs
@@ -0,0 +1,21 @@
+﻿using SF3.Types;
+
+namespace SF3.Tests.MPD.Project {
+    public partial class MPD_ProjectTests {
+        [TestMethod]
+        public void Copy_WithPremiumDisk_MOVSEL_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.PremiumDisk, "MOVSEL");
+
+        [TestMethod]
+        public void Copy_WithPremiumDisk_OPPREM_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.PremiumDisk, "OPPREM");
+
+        [TestMethod]
+        public void Copy_WithPremiumDisk_NIGI01_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.PremiumDisk, "NIGI01");
+
+        [TestMethod]
+        public void Copy_WithPremiumDisk_NIGI02_ProducesSameMPDAsOriginal()
+            => ProducesSameMPDAsOriginal(ScenarioType.PremiumDisk, "NIGI02");
+    }
+}
