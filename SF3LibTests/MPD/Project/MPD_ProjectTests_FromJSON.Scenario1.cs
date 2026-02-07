9f49d1ed2e342a529bf3e843260d318ec8a239ac
Simon Bielman <simon@simonbielman.com>
2026-02-08 00:00:00 +0100

    Added remaining Scenario 1 FromJSON tests (not all pass yet)

--- SF3LibTests/MPD/Project/MPD_ProjectTests_FromJSON.Scenario1.cs
+++ SF3LibTests/MPD/Project/MPD_ProjectTests_FromJSON.Scenario1.cs
@@ -35,7 +35,63 @@ namespace SF3.Tests.MPD.Project {
             => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "BALSA");
 
         [TestMethod]
+        public void FromJSON_WithScenario1_DAM_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "DAM");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_MUCHUR_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "MUCHUR");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_BTL03_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "BTL03");
+
+        [TestMethod]
         public void FromJSON_WithScenario1_BTL02_ProducesSameMPDAsProjectCopy()
             => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "BTL02");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_Z_AS_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "Z_AS");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_GDI_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "GDI");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_MGMA00_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "MGMA00");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_MGMA01_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "MGMA01");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_JOUSAI_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "JOUSAI");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_YAKA3_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "YAKA3");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_SARA03_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "SARA03");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_SARA04_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "SARA04");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_FED06_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "FED06");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_BAKA2_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "BAKA2");
+
+        [TestMethod]
+        public void FromJSON_WithScenario1_HRRAIL_ProducesSameMPDAsProjectCopy()
+            => ProducesSameMPDAsProjectCopyTestBase(ScenarioType.Scenario1, "HRRAIL");
     }
 }
