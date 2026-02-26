a0da9111a47cc36dc17f1dbb1ef353ef885062fa
Simon Bielman <simon@simonbielman.com>
2026-02-27 00:00:00 +0100

    Added position and direction to battle actor editor (non-responsive)

--- SF3LibWin/Controls/ActorBattlePropertiesControl.Designer.cs
+++ SF3LibWin/Controls/ActorBattlePropertiesControl.Designer.cs
@@ -26,6 +26,15 @@ namespace SF3.Win.Controls {
         /// </summary>
         private void InitializeComponent() {
             labelActorEdited = new DarkModeLabel();
+            gbPosition = new DarkModeGroupBox();
+            labelPosition = new DarkModeLabel();
+            nudY = new DarkModeNumericUpDown();
+            nudX = new DarkModeNumericUpDown();
+            cbDirection = new DarkModeComboBox();
+            labelDirection = new DarkModeLabel();
+            gbPosition.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) nudY).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) nudX).BeginInit();
             SuspendLayout();
             // 
             // labelActorEdited
@@ -41,20 +50,97 @@ namespace SF3.Win.Controls {
             labelActorEdited.TabIndex = 3;
             labelActorEdited.Text = "Actor (Battle): (none)";
             // 
+            // gbPosition
+            // 
+            gbPosition.Controls.Add(labelPosition);
+            gbPosition.Controls.Add(nudY);
+            gbPosition.Controls.Add(nudX);
+            gbPosition.Controls.Add(cbDirection);
+            gbPosition.Controls.Add(labelDirection);
+            gbPosition.Location = new System.Drawing.Point(3, 32);
+            gbPosition.Name = "gbPosition";
+            gbPosition.Size = new System.Drawing.Size(200, 83);
+            gbPosition.TabIndex = 4;
+            gbPosition.TabStop = false;
+            gbPosition.Text = "Movement";
+            // 
+            // labelPosition
+            // 
+            labelPosition.AutoSize = true;
+            labelPosition.DisabledColor = System.Drawing.Color.Empty;
+            labelPosition.IsSeparator = false;
+            labelPosition.Location = new System.Drawing.Point(6, 25);
+            labelPosition.Name = "labelPosition";
+            labelPosition.OriginalBorderStyle = System.Windows.Forms.BorderStyle.None;
+            labelPosition.Size = new System.Drawing.Size(53, 15);
+            labelPosition.TabIndex = 5;
+            labelPosition.Text = "Position:";
+            // 
+            // nudY
+            // 
+            nudY.Location = new System.Drawing.Point(136, 23);
+            nudY.Maximum = new decimal(new int[] { 63, 0, 0, 0 });
+            nudY.Name = "nudY";
+            nudY.Size = new System.Drawing.Size(58, 23);
+            nudY.TabIndex = 4;
+            // 
+            // nudX
+            // 
+            nudX.Location = new System.Drawing.Point(73, 23);
+            nudX.Maximum = new decimal(new int[] { 63, 0, 0, 0 });
+            nudX.Name = "nudX";
+            nudX.Size = new System.Drawing.Size(58, 23);
+            nudX.TabIndex = 3;
+            // 
+            // cbDirection
+            // 
+            cbDirection.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            cbDirection.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            cbDirection.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            cbDirection.FormattingEnabled = true;
+            cbDirection.Location = new System.Drawing.Point(73, 51);
+            cbDirection.Name = "cbDirection";
+            cbDirection.Size = new System.Drawing.Size(121, 24);
+            cbDirection.TabIndex = 0;
+            cbDirection.Text = "North";
+            // 
+            // labelDirection
+            // 
+            labelDirection.AutoSize = true;
+            labelDirection.DisabledColor = System.Drawing.Color.Empty;
+            labelDirection.IsSeparator = false;
+            labelDirection.Location = new System.Drawing.Point(6, 54);
+            labelDirection.Name = "labelDirection";
+            labelDirection.OriginalBorderStyle = System.Windows.Forms.BorderStyle.None;
+            labelDirection.Size = new System.Drawing.Size(58, 15);
+            labelDirection.TabIndex = 3;
+            labelDirection.Text = "Direction:";
+            // 
             // ActorBattlePropertiesControl
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(gbPosition);
             Controls.Add(labelActorEdited);
             MaximumSize = new System.Drawing.Size(207, 10000);
             MinimumSize = new System.Drawing.Size(207, 50);
             Name = "ActorBattlePropertiesControl";
-            Size = new System.Drawing.Size(207, 50);
+            Size = new System.Drawing.Size(207, 119);
+            gbPosition.ResumeLayout(false);
+            gbPosition.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize) nudY).EndInit();
+            ((System.ComponentModel.ISupportInitialize) nudX).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
         private DarkModeLabel labelActorEdited;
+        private DarkModeGroupBox gbPosition;
+        private DarkModeNumericUpDown nudY;
+        private DarkModeNumericUpDown nudX;
+        private DarkModeComboBox cbDirection;
+        private DarkModeLabel labelDirection;
+        private DarkModeLabel labelPosition;
     }
 }
--- SF3LibWin/Controls/ActorBattlePropertiesControl.cs
+++ SF3LibWin/Controls/ActorBattlePropertiesControl.cs
@@ -1,6 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Windows.Forms;
+using CommonLib;
 using SF3.Models.Structs.X1.Battle;
+using SF3.Win.Extensions;
 using static SF3.Win.Utils.EventHandlers;
 
 namespace SF3.Win.Controls {
@@ -19,7 +22,22 @@ namespace SF3.Win.Controls {
         }
 
         private void UpdateControls() {
-            labelActorEdited.Text = "Actor (Battle): " + (_actor == null ? "(none)" : $"0x{_actor.ID:X2}");
+            // Guard to prevent tiles from being edited while values are being initialized.
+            if (_nonUserInputGuard > 0)
+                return;
+
+            using (IncrementNonUserInputGuard()) {
+                void InitNUD(NumericUpDown nud, decimal value) {
+                    nud.Value = value;
+                    nud.Text = (nud.Hexadecimal) ? ((int) value).ToString("X") : value.ToString();
+                }
+
+                labelActorEdited.Text = "Actor (Battle): " + (_actor == null ? "(none)" : $"0x{_actor.ID:X2}");
+
+                InitNUD(nudX, _actor.X);
+                InitNUD(nudY, _actor.Z);
+                cbDirection.Text = _actor.Facing.ToString();
+            }
         }
 
         [Browsable(false)]
@@ -30,9 +48,34 @@ namespace SF3.Win.Controls {
                 if (value == _actor)
                     return;
 
+                Control lastFocused = null;
+                if (ContainsFocus && !Focused) {
+                    lastFocused = this.GetFocusedControl();
+                    Focus();
+                }
+
                 _actor = value;
                 UpdateControls();
+
+                if (lastFocused != null) {
+                    if (lastFocused is NumericUpDown nud)
+                        nud.Select(0, nud.Text.Length);
+                    else if (lastFocused is TextBox tb)
+                        tb.SelectAll();
+                    else if (lastFocused is ComboBox cb)
+                        cb.Select(0, cb.Text.Length);
+                }
             }
+        }
+
+        private int _nonUserInputGuard = 0;
+        private ScopeGuard IncrementNonUserInputGuard()
+            => new ScopeGuard(() => _nonUserInputGuard++, () => _nonUserInputGuard--);
+
+        private void DoIfUserInput(Action action) {
+            if (_nonUserInputGuard > 0)
+                return;
+            action();
         }
 
         private Slot _actor = null;
