6f434a527f3ec08c0b60152a613a54ad5dcf3753
Simon Bielman <simon@simonbielman.com>
2026-03-07 00:00:00 +0100

    Fixed DarkModeTextBox handle recreation issue

--- CommonLibWin/Contols/DarkModeTextBox.cs
+++ CommonLibWin/Contols/DarkModeTextBox.cs
@@ -7,30 +7,18 @@ namespace CommonLib.Win.Controls {
     /// TextBox with dark mode support.
     /// </summary>
     public class DarkModeTextBox : TextBox {
+        public DarkModeTextBox() {
+            if (DarkMode.DarkModeContext.Observable.DarkMode)
+                BorderStyle = BorderStyle.FixedSingle;
+        }
+
         protected override void OnHandleCreated(EventArgs e) {
             base.OnHandleCreated(e);
-            if (DarkModeContext == null) {                
+            if (DarkModeContext == null)
                 DarkModeContext = new DarkModeControlContext<DarkModeTextBox>(this);
-
-                OriginalBorderStyle = BorderStyle;
-                if (OriginalBorderStyle != BorderStyle.None && BorderStyle != BorderStyle.FixedSingle) {
-                    DarkModeContext.EnabledChanged += (s, e) => {
-                        if (DarkModeContext.Enabled) {
-                            // Must use BeginInvoke() to prevent call to RecreateHandle() that breaks everything
-                            BeginInvoke(() => this.BorderStyle = BorderStyle.FixedSingle);
-                        }
-                        else {
-                            // Must use BeginInvoke() to prevent call to RecreateHandle() that breaks everything
-                            BeginInvoke(() => this.BorderStyle = OriginalBorderStyle);
-                        }
-                    };
-                }
-
-                DarkModeContext.Init();
-            }
+            DarkModeContext.Init();
         }
 
         private DarkModeControlContext<DarkModeTextBox> DarkModeContext { get; set; }
-        public BorderStyle OriginalBorderStyle { get; private set; }
     }
 }
