3ca9bca0cd30ff8d9ec01f52b2df1b2c1d9fe6ba
Simon Bielman <simon@simonbielman.com>
2026-03-25 00:00:00 +0100

    Start of Palette refactoring

---
+++ CommonLib/Imaging/IPalette.cs
@@ -0,0 +1,29 @@
+﻿namespace CommonLib.Imaging {
+    /// <summary>
+    /// Abstraction of a color palette.
+    /// </summary>
+    public interface IPalette {
+        /// <summary>
+        /// The number of colors in the palette.
+        /// </summary>
+        int ColorCount { get; }
+
+        /// <summary>
+        /// Gets a reference to all colors stored in the palette.
+        /// </summary>
+        PixelChannels[] Channels { get; }
+
+        /// <summary>
+        /// Replaces all colors in the palette.
+        /// </summary>
+        /// <param name="colors">The new channels to replace existing colors with.</param>
+        void Replace(PixelChannels[] colors);
+
+        /// <summary>
+        /// Gets or sets a color in the palette.
+        /// </summary>
+        /// <param name="index">Index of the color to retrieve.</param>
+        /// <returns></returns>
+        PixelChannels this[int index] { get; set; }
+    }
+}
--- CommonLib/Imaging/Palette.cs
+++ CommonLib/Imaging/Palette.cs
@@ -5,13 +5,13 @@ using Newtonsoft.Json.Linq;
 using static CommonLib.Imaging.PixelConversion;
 
 namespace CommonLib.Imaging {
-    public class Palette {
+    public class Palette : IPalette {
         /// <summary>
         /// Converts a palette of colors in ABGR1555 format to an array of PixelChannels[].
         /// </summary>
         /// <param name="colors">An array of colors in ABGR1555 format.</param>
         public Palette(ushort[] colors) {
-            _channels = colors.Select(x => ABGR1555toChannels(x)).ToArray();
+            Channels = colors.Select(x => ABGR1555toChannels(x)).ToArray();
         }
 
         /// <summary>
@@ -19,7 +19,7 @@ namespace CommonLib.Imaging {
         /// </summary>
         /// <param name="palette">Palette to copy.</param>
         public Palette(Palette palette) {
-            _channels = palette.Channels.Clone() as PixelChannels[];
+            Channels = palette.Channels.Clone() as PixelChannels[];
         }
 
         /// <summary>
@@ -27,7 +27,7 @@ namespace CommonLib.Imaging {
         /// </summary>
         /// <param name="channels">Color channels to copy.</param>
         public Palette(PixelChannels[] channels) {
-            _channels = channels.Clone() as PixelChannels[];
+            Channels = channels.Clone() as PixelChannels[];
         }
 
         /// <summary>
@@ -35,30 +35,30 @@ namespace CommonLib.Imaging {
         /// </summary>
         /// <param name="colorCount">The number of colors.</param>
         public Palette(int colorCount) {
-            _channels = new PixelChannels[colorCount];
+            Channels = new PixelChannels[colorCount];
             for (var i = 0; i < colorCount; i++) {
                 var color = (byte) ((255 * i) / colorCount);
-                _channels[i] = new PixelChannels() { A = 255, R = color, G = color, B = color };
+                Channels[i] = new PixelChannels() { A = 255, R = color, G = color, B = color };
             }
         }
 
         public static Palette FromJToken(JToken token) => new Palette(token);
         private Palette(JToken token) {
             var jArray = (JArray) token;
-            _channels = jArray.Select(x => PixelChannels.FromHtmlColor((string) x, (byte) 0)).ToArray();
+            Channels = jArray.Select(x => PixelChannels.FromHtmlColor((string) x, (byte) 0)).ToArray();
         }
 
-        private readonly PixelChannels[] _channels;
-        public PixelChannels[] Channels {
-            get => _channels;
-            set {
-                if (value == null)
-                    throw new ArgumentNullException(nameof(Channels));
-                if (value.Length != _channels.Length)
-                    throw new ArgumentException($"Wrong length for '{nameof(Channels)}'");
-                for (int i = 0; i < value.Length; i++)
-                    _channels[i] = value[i];
-            }
+        public int ColorCount => Channels.Length;
+
+        public PixelChannels[] Channels { get; }
+
+        public void Replace(PixelChannels[] colors) {
+            if (colors == null)
+                throw new ArgumentNullException(nameof(Channels));
+            if (colors.Length != Channels.Length)
+                throw new ArgumentException($"Wrong length for '{nameof(Channels)}'");
+            for (int i = 0; i < colors.Length; i++)
+                Channels[i] = colors[i];
         }
 
         public PixelChannels this[int index] {
