04f0cebbd26f8877d9c3ac0b688c49a0994ea5fd
Simon Bielman <simon@simonbielman.com>
2026-03-28 00:00:00 +0100

    Fix for a bug where showing the tooltip in ToolStripButton prevents GC of the MPD_ViewerControl

--- CommonLibWin/Contols/DarkModeToolStrip.cs
+++ CommonLibWin/Contols/DarkModeToolStrip.cs
@@ -17,6 +17,27 @@ namespace CommonLib.Win.Controls {
             }
         }
 
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                // If the button tooltip was shown, this creates a strong reference to this control that remains after
+                // disposal. Forcing ToolTipText to 'null' will remove any refs to any button a tooltip might have.
+                //
+                // (This was happening with a Form that had a ToolTip with a reference to a nested button that was
+                // preventing a huge object from being garbage collected D:)
+                //
+                // There's also a nasty memory leak that occurs when the mouse is hovering over a button while the
+                // control is removed... This doesn't seem to be fixed by this... C'mon, Microsoft!
+                ShowItemToolTips = false;
+                foreach (var item in Items) {
+                    if (item is ToolStripButton button) {
+                        button.ToolTipText = null;
+                        button.AutoToolTip = false;
+                    }
+                }
+            }
+            base.Dispose(disposing);
+        }
+
         private DarkModeToolStripContext<DarkModeToolStrip> DarkModeContext { get; set; }
     }
 }
