7e37713ee318599bcce988a675beadb33236f1bc
Simon Bielman <simon@simonbielman.com>
2026-08-24 17:10:46 +0200

    Alternate non-base SGL_ModelInstance with non-abstract getter

---
+++ CommonLib/SGL/SGL_ModelInstance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace CommonLib.SGL {
+    public class SGL_ModelInstance : SGL_ModelInstanceBase {
+        public SGL_ModelInstance(Func<SGL_ModelInstance, int /*lod*/, ISGL_Model> modelGetter) : base() {
+            ModelGetter = modelGetter;
+        }
+
+        public SGL_ModelInstance(ISGL_ModelInstance original, Func<SGL_ModelInstance, int /*lod*/, ISGL_Model> modelGetter) : base(original) {
+            ModelGetter = modelGetter;
+        }
+
+        public static SGL_ModelInstance FromJToken(JToken token, Func<SGL_ModelInstance, int /*lod*/, ISGL_Model> modelGetter) => new SGL_ModelInstance(token, modelGetter);
+        protected SGL_ModelInstance(JToken token, Func<SGL_ModelInstance, int /*lod*/, ISGL_Model> modelGetter) : base(token) {
+            ModelGetter = modelGetter;
+        }
+
+        public override ISGL_Model GetModel(int lod)
+            => ModelGetter?.Invoke(this, lod);
+
+        public Func<SGL_ModelInstance, int /*lod*/, ISGL_Model> ModelGetter { get; set; }
+    }
+}
