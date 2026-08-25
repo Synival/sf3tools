59241d917a21145e3da3d199e0b26f4f68ba6f5b
Simon Bielman <simon@simonbielman.com>
2026-08-25 19:00:20 +0200

    Refactored MPD_PDataStruct to use general SGL_ModelStruct

--- SF3Lib/Models/Structs/MPD/Model/MPD_PDataStruct.cs
+++ SF3Lib/Models/Structs/MPD/Model/MPD_PDataStruct.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using CommonLib.Attributes;
 using CommonLib.SGL;
@@ -11,7 +10,7 @@ using SF3.MPD.Interfaces;
 using SF3.Types;
 
 namespace SF3.Models.Structs.MPD.Model {
-    public class MPD_PDataStruct : PDataStruct, IMPD_ModelLoD {
+    public class MPD_PDataStruct : SGL_Model_PDataStruct, IMPD_ModelLoD {
         public MPD_PDataStruct(IByteData data, int id, string name, int address,
             MPD_CollectionType collection, IMPD_File mpdFile, int? chunkIndex, int modelId, int lod, int refs
         ) : base(data, id, name, address, 0x14) {
@@ -21,13 +20,9 @@ namespace SF3.Models.Structs.MPD.Model {
             ModelID       = modelId;
             LevelOfDetail = lod;
             Refs          = refs;
-
-            var faceCount = FaceCount;
-            var mockFaces = Enumerable.Range(0, faceCount).Select(x => new MockFace(this, x)).ToArray();
-            Faces = new MockFaceEnumerable(mockFaces);
         }
 
-        public int ModelCollectionID => (int) Collection;
+        public override int ModelCollectionID => (int) Collection;
 
         [TableViewModelColumn(addressField: null, displayOrder: -2.66f, displayName: "Collection", minWidth: 110)]
         public MPD_CollectionType Collection { get; }
@@ -47,10 +42,10 @@ namespace SF3.Models.Structs.MPD.Model {
         public int? ChunkIndex { get; }
 
         [TableViewModelColumn(addressField: null, displayOrder: -2.15f, displayFormat: "X2")]
-        public int ModelID { get; }
+        public override int ModelID { get; }
 
         [TableViewModelColumn(addressField: null, displayOrder: -2.14f)]
-        public int LevelOfDetail { get; }
+        public override int LevelOfDetail { get; }
 
         [TableViewModelColumn(addressField: null, displayOrder: -0.2f)]
         public int Refs { get; }
@@ -58,74 +53,8 @@ namespace SF3.Models.Structs.MPD.Model {
         [TableViewModelColumn(addressField: null, displayOrder: -0.1f, isPointer: true, isReadOnly: true)]
         public uint RamAddress { get; set; }
 
-        private class MockFace : ISGL_ModelFace {
-            public MockFace(MPD_PDataStruct pdata, int index) {
-                PData = pdata;
-                Index = index;
-            }
-
-            public IReadOnlyList<int> VertexIndices => Polygon?.Vertices;
-
-            public VECTOR Normal {
-                get {
-                    var poly = Polygon;
-                    return poly == null ? new VECTOR() : new VECTOR(poly.NormalX, poly.NormalY, poly.NormalZ);
-                }
-                set {
-                    var poly = Polygon;
-                    if (poly != null && value != null) {
-                        poly.NormalX = value.X.Float;
-                        poly.NormalY = value.Y.Float;
-                        poly.NormalZ = value.Z.Float;
-                    }
-                }
-            }
-
-            public IATTR Attributes {
-                get {
-                    var attr = AttributeStruct;
-                    return attr == null ? new ATTR() : (IATTR) attr;
-                }
-                set {
-                    var attr = AttributeStruct;
-                    if (attr != null && value != null) {
-                        attr.Plane          = value.Plane;
-                        attr.SortAndOptions = value.SortAndOptions;
-                        attr.TextureNo      = value.TextureNo;
-                        attr.Mode           = value.Mode;
-                        attr.ColorNo        = value.ColorNo;
-                        attr.GouraudShadingTable = value.GouraudShadingTable;
-                        attr.Dir            = value.Dir;
-                    }
-                }
-            }
-
-            public readonly MPD_PDataStruct PData;
-            public readonly int Index;
-
-            public PolygonStruct Polygon => ((Index < PData.Polygons?.Count) == true) ? PData.Polygons[Index] : null;
-            public AttrStruct AttributeStruct => ((Index < PData.Attributes?.Count) == true) ? PData.Attributes[Index] : null;
-        }
-
-        private class MockFaceEnumerable : IReadOnlyList<ISGL_ModelFace> {
-            public MockFaceEnumerable(MockFace[] faces) {
-                Faces = faces;
-            }
-
-            public MockFace[] Faces { get; }
-
-            public int Count => Faces.Length;
-            public ISGL_ModelFace this[int index] => Faces[index];
-            public ISGL_ModelFace[] AsArray() => Faces;
-
-            public IEnumerator<ISGL_ModelFace> GetEnumerator() => ((IEnumerable<ISGL_ModelFace>) Faces).GetEnumerator();
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }
-
-        public PolygonTable Polygons => (Chunk?.PolygonTablesByMemoryAddress?.TryGetValue(PolygonsOffset, out var polygons) == true) ? polygons : null;
-        public AttrTable Attributes => (Chunk?.AttrTablesByMemoryAddress?.TryGetValue(AttributesOffset, out var attributes) == true) ? attributes : null;
-
-        public IReadOnlyList<VECTOR> Vertices => (Chunk?.VertexTablesByMemoryAddress?.TryGetValue(VerticesOffset, out var vertices) == true) ? vertices : null;
-        public IReadOnlyList<ISGL_ModelFace> Faces { get; }
+        public override PolygonTable Polygons => (Chunk?.PolygonTablesByMemoryAddress?.TryGetValue(PolygonsOffset, out var polygons) == true) ? polygons : null;
+        public override AttrTable Attributes => (Chunk?.AttrTablesByMemoryAddress?.TryGetValue(AttributesOffset, out var attributes) == true) ? attributes : null;
+        public override IReadOnlyList<VECTOR> Vertices => (Chunk?.VertexTablesByMemoryAddress?.TryGetValue(VerticesOffset, out var vertices) == true) ? vertices : null;
     }
 }
---
+++ SF3Lib/Models/Structs/Shared/SGL/SGL_Model_PDataStruct.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using CommonLib.SGL;
+using SF3.ByteData;
+using SF3.Models.Tables.Shared.SGL;
+
+namespace SF3.Models.Structs.Shared.SGL {
+    public abstract class SGL_Model_PDataStruct : PDataStruct, ISGL_Model {
+        protected SGL_Model_PDataStruct(IByteData data, int id, string name, int address) : base(data, id, name, address) {
+            var faceCount = FaceCount;
+            var mockFaces = Enumerable.Range(0, faceCount).Select(x => new MockFace(this, x)).ToArray();
+            Faces = new MockFaceEnumerable(mockFaces);
+        }
+
+        protected SGL_Model_PDataStruct(IByteData data, int id, string name, int address, int size) : base(data, id, name, address, size) {
+            var faceCount = FaceCount;
+            var mockFaces = Enumerable.Range(0, faceCount).Select(x => new MockFace(this, x)).ToArray();
+            Faces = new MockFaceEnumerable(mockFaces);
+        }
+
+        private class MockFace : ISGL_ModelFace {
+            public MockFace(SGL_Model_PDataStruct sglModel, int index) {
+                SGL_Model = sglModel;
+                Index = index;
+            }
+
+            public IReadOnlyList<int> VertexIndices => Polygon?.Vertices;
+
+            public VECTOR Normal {
+                get {
+                    var poly = Polygon;
+                    return poly == null ? new VECTOR() : new VECTOR(poly.NormalX, poly.NormalY, poly.NormalZ);
+                }
+                set {
+                    var poly = Polygon;
+                    if (poly != null && value != null) {
+                        poly.NormalX = value.X.Float;
+                        poly.NormalY = value.Y.Float;
+                        poly.NormalZ = value.Z.Float;
+                    }
+                }
+            }
+
+            public IATTR Attributes {
+                get {
+                    var attr = AttributeStruct;
+                    return attr == null ? new ATTR() : (IATTR) attr;
+                }
+                set {
+                    var attr = AttributeStruct;
+                    if (attr != null && value != null) {
+                        attr.Plane          = value.Plane;
+                        attr.SortAndOptions = value.SortAndOptions;
+                        attr.TextureNo      = value.TextureNo;
+                        attr.Mode           = value.Mode;
+                        attr.ColorNo        = value.ColorNo;
+                        attr.GouraudShadingTable = value.GouraudShadingTable;
+                        attr.Dir            = value.Dir;
+                    }
+                }
+            }
+
+            public readonly SGL_Model_PDataStruct SGL_Model;
+            public readonly int Index;
+
+            public PolygonStruct Polygon => ((Index < SGL_Model.Polygons?.Count) == true) ? SGL_Model.Polygons[Index] : null;
+            public AttrStruct AttributeStruct => ((Index < SGL_Model.Attributes?.Count) == true) ? SGL_Model.Attributes[Index] : null;
+        }
+
+        private class MockFaceEnumerable : IReadOnlyList<ISGL_ModelFace> {
+            public MockFaceEnumerable(MockFace[] faces) {
+                Faces = faces;
+            }
+
+            public MockFace[] Faces { get; }
+
+            public int Count => Faces.Length;
+            public ISGL_ModelFace this[int index] => Faces[index];
+            public ISGL_ModelFace[] AsArray() => Faces;
+
+            public IEnumerator<ISGL_ModelFace> GetEnumerator() => ((IEnumerable<ISGL_ModelFace>) Faces).GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        public abstract int ModelCollectionID { get; }
+        public abstract int ModelID { get; }
+        public abstract int LevelOfDetail { get; }
+
+        public abstract PolygonTable Polygons { get; }
+        public abstract AttrTable Attributes { get; }
+        public abstract IReadOnlyList<VECTOR> Vertices { get; }
+
+        public IReadOnlyList<ISGL_ModelFace> Faces { get; }
+    }
+}
