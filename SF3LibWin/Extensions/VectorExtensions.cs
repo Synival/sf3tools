fe472033a3a055661a41da53f7ea31dd61a0d694
Simon Bielman <simon@simonbielman.com>
2026-09-09 20:21:00 +0200

    Added more vector conversion extensions

--- SF3LibWin/Extensions/VectorExtensions.cs
+++ SF3LibWin/Extensions/VectorExtensions.cs
@@ -17,5 +17,30 @@ namespace SF3.Win.Extensions {
 
         public static Vector4 ToVector4(this VECTOR vec)
             => new(vec.X.Float, vec.Y.Float, vec.Z.Float, 1);
+
+        public static Vector2 ToOpenTKVector(this System.Numerics.Vector2 vec)
+            => new(vec.X, vec.Y);
+
+        public static Vector3 ToOpenTKVector(this System.Numerics.Vector3 vec)
+            => new(vec.X, vec.Y, vec.Z);
+
+        public static Vector4 ToOpenTKVector(this System.Numerics.Vector4 vec)
+            => new(vec.X, vec.Y, vec.Z, vec.W);
+
+        public static System.Numerics.Vector2 ToNumericsVector(this Vector2 vec)
+            => new(vec.X, vec.Y);
+
+        public static System.Numerics.Vector3 ToNumericsVector(this Vector3 vec)
+            => new(vec.X, vec.Y, vec.Z);
+
+        public static System.Numerics.Vector4 ToNumericsVector(this Vector4 vec)
+            => new(vec.X, vec.Y, vec.Z, vec.W);
+
+        public static VECTOR ToVECTOR(this System.Numerics.Vector3 vec)
+            => new(vec.X, vec.Y, vec.Z);
+
+        public static VECTOR ToVECTOR(this Vector3 vec)
+            => new(vec.X, vec.Y, vec.Z);
+
     }
 }
